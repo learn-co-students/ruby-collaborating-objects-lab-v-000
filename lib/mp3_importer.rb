4638fc800d9cac3a5cebbd522e76d352786284b7
Justin Thomas <jthomas0209@gmail.com>
2015-11-21 12:30:58 -0500

    Defined Classes Song, Artist, & MP3Importer

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class MP3Importer
+  attr_accessor :path
+  attr_accessor :file_array
+  
+  def initialize(filepath)
+    @path = filepath
+  end
+
+  def files
+    @file_array = Dir.entries(@path).select{|file| file.include?(".mp3")}
+  end
+
+  def import
+    self.files
+    @file_array.each{|song| Song.new_by_filename(song)}
+  end
+
+end
