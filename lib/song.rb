0aa689252c8ae02c4cd6b8a2760ea9ddb3fd4675
ed-karabinus <ejkarabin@gmail.com>
2015-12-05 00:07:54 -0600

    Added helper method Song#artist_name= to song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,11 @@ class Song
   def self.new_by_filename(file_name)
     data = file_name.split(/(\.| - )/)
     song = Song.new(data[2])
-    song.artist = Artist.find_or_create_by_name(data[0])
+    song.artist_name = data[0]
     song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
   end
 end
