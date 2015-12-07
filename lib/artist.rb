beed4c3d4c7055c4e151aa043942791b9630d581
lisamarie616 <lisamarie616@gmail.com>
2015-12-07 10:27:21 -0800

    add Artist, Song, and MP3Importer classes with object relationships

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| artist.name == name} || artist = self.new(name)
+  end
+
+  def print_songs
+    puts @songs
+  end
+
+end
