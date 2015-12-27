04ee0cebc7912ec6535ab83d4aebcb219c6639b7
eric-an <erican@me.com>
2015-12-26 18:20:53 -0800

    create classes with collaborations and associations

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files 
+    Dir.entries(path).select {|name| name.end_with?(".mp3")}
+  end
+
+  def import
+    files.each { |file_name| Song.new_by_filename(file_name) }
+  end
+end
