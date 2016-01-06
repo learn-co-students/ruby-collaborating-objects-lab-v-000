bfad0d0a1a5c0f0d00e49f7fa0193f93ce833ee1
pacosta613 <pedro.acosta613@gmail.com>
2016-01-06 15:09:36 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist
 
@@ -5,8 +6,15 @@ class Song
     @name = name
   end
 
-  def self.new_by_filename(name)
-    
+  def self.new_by_filename(filename)
+    song = self.new(filename.split(" - ")[1])
+    song.artist_name = filename.split(" - ")[0]
+    song
+  end
+
+  def artist_name=(filename)
+    artists = Artist.find_or_create_by_name(filename)
+    @artist = artists
   end
 
 end
