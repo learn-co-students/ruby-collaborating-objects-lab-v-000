f8e9e0a7d0bf0d22593bb5db77fdea238a130377
Heber Sandoval <he3ers@gmail.com>
2016-02-02 15:48:03 -0500

    Create a Artist, Song and MP3Importer class

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+   attr_accessor :name, :artist
+
+   def initialize(name)
+     @name = name
+   end
+
+   def self.new_by_filename(file)
+     new_file = file.split(" - ")
+     song_name = new_file[1]
+     artist_name = new_file[0]
+     new_song = Song.new(song_name)
+     new_song.artist = Artist.find_or_create_by_name(artist_name)
+     new_song
+   end
+
+ end
