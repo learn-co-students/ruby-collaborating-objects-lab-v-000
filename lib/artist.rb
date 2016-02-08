ede081369c407bbf56ab34c0309030db3c69c86b
aprietof <adrianprieto00@gmail.com>
2016-02-07 22:13:34 -0500

    Done

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,7 @@ class Artist
   end
 
   def self.find_by_name(name)
-    @@all.detect{|artist| artist.name == name}
+    self.all.detect{|artist| artist.name == name}
   end
 
   def self.create_by_name(name)
