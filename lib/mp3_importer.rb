f119103bc5e446e8ba89e11be0fc3ae148ca9600
wsaxe <geosaxe@gmail.com>
2016-02-20 00:33:50 -0600

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class MP3Importer
+  attr_accessor :path
+  
+  def initialize(path)
+    self.path = path
+  end
+  
+  def files
+    path = self.path + "/*.mp3"
+    processed = Dir[path].collect do |path_and_name|
+      path_and_name.split("/").last
+    end
+    processed
+  end
+  
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+end
