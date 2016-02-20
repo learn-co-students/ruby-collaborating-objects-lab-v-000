9e5ced2472dba421673e4aa25a22a342986dff32
reifnotreef <bridgerrhammond@gmail.com>
2016-02-19 20:20:55 -0600

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   end
 
   def self.new_by_filename(file)
-    no_extension = file.split(/\A(.+)\s-\s(.+)\s-\s(.+).mp3\z/) # I hate regex with a passion. I found compiled this from a few different stack threads and I'm still not sure how it works.
+    no_extension = file.split(/\A(.+)\s-\s(.+)\s-\s(.+).mp3\z/) # I hate regex with a passion. I found/compiled this from a few different stack threads and I'm still not sure how it works.
     new_song = Song.new(no_extension[2])
     new_song.artist_name=(no_extension[1])
     new_song
