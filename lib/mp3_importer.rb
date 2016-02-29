71b69bd50b12f96d3ca7540d51ae3ed8e49526cb
jmaerk235 <cmerrick@mail.fresnostate.edu>
2016-02-29 11:05:09 -0800

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,7 @@ class MP3Importer
     files
   end
 
-  def files #loads all the mp3 files in the path directory
+  def files #loads all the mp3 files in the path array
     file_array = []
     Dir.entries(@path).each do |entry|
       if entry.include?("mp3")
@@ -24,14 +24,3 @@ class MP3Importer
   end
 
 end
-
-#def files
-#  #loads all the mp3 files in the path directory
-#  #normalizes the filename to just the mp3 filename with no path
-#  @parsed = @path.split("-")
-#end
-#
-#  #imports the files into the library by creating songs from a filename 
-#def import(list_of_filenames)
-#  list_of_filenames.each{|file_name| songs << Song.new_by_filename(file_name)}
-#end
