53986a388fe85800dc8c9faa75c34451bc6c041d
jbok4 <jbok4@aol.com>
2016-03-04 18:36:48 -0500

    one last edit

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,6 @@ class Song
     def self.new_by_filename(x) #instance method
     artist, song = x.split(" - ")
     new_song = self.new(song)
-    #song.name = song_name
     new_song.artist_name = artist
     new_song
   end
