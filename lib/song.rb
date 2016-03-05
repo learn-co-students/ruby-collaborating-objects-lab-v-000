54a52c592d4051efd66088d8d703f6a07d344fbc
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-03-05 08:28:44 -0800

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,9 @@ class Song
     @artist = artist
     @title = title
   end
-  def self.new_by_filename
-    name = self.split(" - ")[0]
-    title = self.split(" - ")[1]
+  def self.new_by_filename(file)
+    name = file.split(" - ")[0]
+    title = file.split(" - ")[1]
     song = [name, title]
     song = self.new
   end
