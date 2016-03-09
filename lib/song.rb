af87e29442a4fa64315d827e7eac7105fb815cdb
mcclains2003 <mcclains2003@gmail.com>
2016-03-09 13:02:29 -0600

    Getting help with artist=

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+  end
+
+  def artist=(string)
+    obj = Artist.find_or_create_by_name(string)
+    obj.add_song(self)
+  end
+
+  def self.new_by_filename(file_name)
+    info = file_name.split(" - ")
+    song = Song.new(info[1])
+    song.artist = info[0]
+  end
+end
