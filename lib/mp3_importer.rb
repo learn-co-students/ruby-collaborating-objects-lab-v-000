41c7198925bacd6691f1f58988112ed19ef3c61a
johnsontaiwo <josseytee@gmail.com>
2016-03-20 14:52:08 -0400

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 class MP3Importer
   attr_accessor :path
   
@@ -9,12 +9,15 @@ class MP3Importer
   def files
     Dir.entries(path).select do |file|
        file.end_with?(".mp3")
+       
         end
   end
   
    
    def import
+    
     files.each{ |file|  Song.new_by_filename(file)}
     
    end
 end
+  
