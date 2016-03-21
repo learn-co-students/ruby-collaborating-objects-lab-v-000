6009c3b71f800a97580f08eecb68169d5486e9be
rafeeq-123 <rafeeq-123@hotmail.com>
2016-03-21 11:39:47 -0700

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,53 @@
+class Artist
+attr_accessor :name, :songs, :artist
+@@all=[]
+
+def initialize(name)
+  @name=name
+  @artist=artist
+  @songs=[]
+
+end
+
+def add_song(song)
+  @songs << song
+end
+def save
+@@all << artist
+
+end
+ 
+def self.find_or_create_by_name(artists_name_here)
+
+  artist=Artist.new(" ")
+  
+      
+  
+
+      
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+attr_accessor :name, :artist
+
+def initialize(name)
+    @name=name
+    @artist=artist
+
+end
+
+def artist_name=(name)
+  song=Artist.new(name)
+  
+
+
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
