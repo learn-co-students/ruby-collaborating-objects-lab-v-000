429c79554670fc49df3cb06de2340f12e4d4d22e
github username <github email address>
2016-03-28 19:23:09 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,6 @@ class Song
 	def self.new_by_filename(filename)
 		data = filename.split(" - ").collect {|x| x.gsub(/.mp3/, '')}
 	  	song = self.new(data[1])
-  		song.name = data[1]
 		artist = Artist.find_or_create_by_name(data[0])
 		song.artist = artist
   		artist.name = data[0]
