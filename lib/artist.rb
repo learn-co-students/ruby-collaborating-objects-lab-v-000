0cc70b5bda3116f3fc844ee3cb9ba4f15467d961
ARAF91 <araft50@yahoo.com>
2016-04-07 11:52:55 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+class Artist
+
+  attr_accessor :name
+  @@all=[]
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+     artist = Artist.new(name)
+     artist.save
+     artist
+  end
+
+   def self.find_by_name(a_name)
+     self.all.detect{|a| a.name == a_name}
+   end
+
+   def self.find_or_create_by_name(a_name)
+     self.find_by_name(a_name) || self.create(a_name)
+   end
+
+  def print_songs
+    puts "#{@songs.collect{|s| s.name}.join("\n")}"
+  end
+end
