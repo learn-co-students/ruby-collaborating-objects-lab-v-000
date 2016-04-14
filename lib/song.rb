4b3d05918ca7752b97f86bd35dd8df0714ff7a69
hangloos <ericloos00@gmail.com>
2016-04-14 16:51:00 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,7 @@ def self.new_by_filename(file_name)
   file = file_name.split(" - ")
   new_song = Song.new(file[1])
   new_song.artist = Artist.find_or_create_by_name(file[0])
+  new_song
 end
 
 
