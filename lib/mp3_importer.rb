610610ed155438a45b94f06a6f2aab0e39b663fd
thebrianemory <the@brianemory.com>
2016-04-17 07:34:30 -0700

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class MP3Importer
+  attr_accessor :songs, :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.entries(path).select { |x| x[/\.mp3$/] }
+  end
+
+  def import
+    i = 0
+    while i < self.files.length
+      Song.new_by_filename(self.files[i])
+      i += 1
+    end
+  end
+
+end
+
+# binding.pry
