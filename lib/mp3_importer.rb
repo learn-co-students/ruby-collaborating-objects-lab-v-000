3bcbc762c9a22701d42d002f68a4bed118539085
nicolefederici <nicolefederici@yahoo.com>
2016-05-06 01:28:31 -0400

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,31 @@
+class MP3Importer
+  attr_accessor :songs,:path
+
+  
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files = Dir.foreach(@path).select {|x| File.file?("#{@path}/#{x}")}
+    @files
+  end
+
+
+
+  def import 
+
+    self.files.each do |file_name| 
+      new_song = Song.new_by_filename(file_name)
+      
+    end
+
+  end
+     
+  
+
+
+
+
+end
