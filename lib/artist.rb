b4bbe57b949baea5f11b22eb2e8b37fcb76ae35d
WilliamMena <williammena1991@gmail.com>
2016-05-08 16:08:15 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
+require'pry'
+
 class Artist
-
-
 
   attr_accessor :name, :artist, :songs, :all
 
@@ -10,11 +10,23 @@ class Artist
     @@all = []
   end
 
+  def self.find(name)
+    self.all.find {|artist| artist.name == name}
+  end
+
+  def self.create(name)
+    #self.new(artist)
+    #save
+    self.new(name).tap {|artist| artist.save}
+  end
+
+
   def self.find_or_create_by_name(artist)
-    if self.name == nil
-      self.name = Artist.new(artist)
+    if self.find(artist) == nil
+      self.create(artist)
     else
-      self.name = artist
+      #self.name = artist
+      self.find(artist)
     end
   end
 
@@ -30,6 +42,9 @@ class Artist
     @@all << self
   end
 
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
 
 
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require'pry'
 class Song
 
   attr_accessor :name, :title, :artist
@@ -8,13 +9,26 @@ class Song
 
   def self.artist=(artist)
     Artist.find_or_create_by_name(artist)
-
   end
+
+  #def self.artist(name)
+  #  if self.artist == nil
+  #    self.artist = Artist.new(name)
+  #  else
+  #    self.artist.name = name
+  #  end
+  #end
 
   def self.new_by_filename(file_name)
     title = file_name.split(" - ")[1]
-    self.new.artist.name = title
+    name = file_name.split(" - ")[0]
+    #binding.pry
+    self.new(title)
+    #binding.pry
+    self.artist.name = name
   end
+
+
 
 
 end
