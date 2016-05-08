354f3049faa36c81c0242b35e53a4b2e81e97833
WilliamMena <williammena1991@gmail.com>
2016-05-08 18:32:09 -0400

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,22 +4,20 @@ class MP3Importer
 
   def initialize(path)
     @path = path
-
+    @path << "/*.mp3"
   end
 
   def files
-    @path << "/*.mp3"
+
     @files = Dir.glob(@path)
     @files = @files.each {|songs| songs.slice! "./spec/fixtures/mp3s/" }
+    #binding.pry
     @files
   end
 
   def import
-    @path << "/*.mp3"
-    @song_list = Dir.glob(@path)
-    @song_list = @song_list.each {|songs| Song.new_by_filename(songs) }
-    #Song.new_by_filename(song_list)
-    #binding.pry
+    files
+    @files.each {|songs| Song.new_by_filename(songs) }
   end
 
 
