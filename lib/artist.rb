38de4002ec5317b32b0c6791d654741eb14db356
Dax Blemberg <dax.blem@gmail.com>
2016-05-12 21:42:19 -0400

    finished

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    self.name = name
+    self.songs = []
+    self.save
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def print_songs
+    self.songs.each { |song| puts song.name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    artist = @@all.find { |artist| artist.name == name }
+    artist = self.new(name) if artist.nil?
+    artist
+  end
+
+end
