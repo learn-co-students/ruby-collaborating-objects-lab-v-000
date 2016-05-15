1181a553af8362f7ed84cc7b23ea09c19c36149b
Jason <jasonkwong11@gmail.com>
2016-05-15 19:55:24 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,6 +8,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    self.save
   end
 
   def songs
@@ -23,19 +24,17 @@ class Artist
   end
 
   def save
-    @@all << self
+    @@all << self 
   end
 
 def Artist.find_or_create_by_name(name)
-  artist_instance = ""
-
- artist_instance = @@all.detect(Artist.new(name).save){ |artist_instance| artist_instance.name == name}
-
+   @@all.detect { |artist| artist.name == name } || Artist.new(name)
 end
 
-  def print_songs
-    @songs.each {|song| puts song.name}
-  end
+
+def print_songs
+ @songs.each {|song| puts song.name}
+end
 
 
 end
