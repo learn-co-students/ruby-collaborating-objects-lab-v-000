a91728edcdabbd5983c5cf148f58ad240ff0b1f7
ConnorMarble <connor.marble@gmail.com>
2016-05-21 19:49:40 -0400

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+ attr_accessor :path
+
+  def initialize(file_path)
+    @path = file_path
+  end
+
+   def files
+     files = Dir["#{path}/*.mp3"]
+     files.map { |f| File.basename(f) }
+   end
+ 
+   def import
+     files.each { |f| Song.new_by_filename(f) }
+   end
+ end
