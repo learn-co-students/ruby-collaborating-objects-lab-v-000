507cb2a8bc2eb9d8bfaedd042a50d02010eda9da
Being Young <beingy@gmail.com>
2016-05-21 18:53:00 -0400

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+class MP3Importer
+  attr_accessor :files, :path
+
+  def initialize(file_path)
+    self.path = file_path
+    self.files = Dir.entries(self.path).select {|file| file.include?("mp3") }
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+
+end
