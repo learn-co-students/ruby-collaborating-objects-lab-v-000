bba36670f0caf01e746489ac104596c2ef365537
joelbitar1986 <joelbitar1986@gmail.com>
2016-05-22 20:44:28 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,6 @@ class Song
 
   def self.new_by_filename(filename)
     artist, name = filename.split(" - ")
-    binding.pry
     new_song = self.new(name)
     new_song.artist_name = artist
     new_song
