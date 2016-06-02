ad32d0d78eb7fa02854609d1fc3d0384db19480c
bjacooper <bjacooper@gmail.com>
2016-06-02 17:10:00 +0100

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,12 +6,16 @@ class Artist
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def self.all
     @@all
   end
 
+  def add_song(song)
+    @songs << song
+  end
 
   def save
     @@all << self
