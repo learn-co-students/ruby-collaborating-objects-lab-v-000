ef504a656544229873e4b2217e7c6765397465f2
bmolina-nyc <bruce.d.molina@gmail.com>
2016-06-03 22:07:50 -0400

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,28 +2,19 @@ class MP3Importer
 
   attr_accessor :path 
 
-  @@all_pre = [] 
-  @@all = []
-  @@files = []
   def initialize(path)
     @path = path
-    @@files << Dir.entries(path).select {|el| el.include?("mp3")}
+    @files = Dir.entries(path).select {|el| el.include?("mp3")}
   end
-
 
 
   def files
-    @@files.each do |file_arr|
-      file_arr.each do |file_el|
-        @@all_pre << file_el unless @@all.include?(file_el)
-      end
-    end
-    @@all_pre.uniq!.compact 
+    @files
   end
 
   def  import 
-    @@all_pre.each do |filename|
-     Song.new_by_filename(filename)
+    files.each do |filename|
+    Song.new_by_filename(filename)
     end
   end
 
--- lib/song.rb
+++ lib/song.rb
@@ -8,13 +8,13 @@ class Song
 
   def self.new_by_filename(filename)
     song = Song.new(filename.split(" - ")[1])
-    song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
+    song.artist_name = (filename.split(" - ")[0])
     song
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
+    if self.artist.nil?
+      self.artist = Artist.find_or_create_by_name(name)
     else
       self.artist.name = name
     end
