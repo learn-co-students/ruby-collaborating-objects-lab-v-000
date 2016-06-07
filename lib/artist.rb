57df04a9b32e0f010b2fefcb70ca280b1c62b258
Dom McKellar <dom.mckellar@gmail.com>
2016-06-07 10:29:48 -0700

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,8 @@ class Artist
 # Artist will be responsible for either creating the artist (if the artist doesn't exist in our program yet) or finding the instance of that artist (if the artist does exist).
 
   @@all = []
-  attr_accessor :name, :songs
+  attr_accessor :name#, :songs
+  attr_reader :songs
 
   def initialize(name)
     @name = name
