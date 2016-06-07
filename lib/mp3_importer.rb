e1378240c190ab4f5a4f69886c510cdf0dbea6a9
bjacooper <bjacooper@gmail.com>
2016-06-07 18:35:15 +0100

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,14 +1,21 @@
+require 'pry'
+
 class MP3Importer
 
-  def initialize
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
   end
 
   def files
+    Dir.entries(@path).select {|f| !File.directory? f}
   end
 
   def import
+    files.collect do |filename|
+      filename.split(" - ").to_a[2]
+    end
   end
-
-
 
 end
