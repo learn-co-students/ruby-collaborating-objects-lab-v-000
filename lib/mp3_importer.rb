c910fc84477af19750dd15a974a5aa2998a2323e
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-12 18:58:11 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,6 @@
 require "pry"
 class MP3Importer
-
+attr_accessor :files
 attr_reader :path
 
   def initialize(path) #accepts a file path to parse mp3 files from
@@ -8,14 +8,14 @@ attr_reader :path
   end
 
   def files
-    files = Dir.entries(path) #put the list of files in the files directory!
-    files.delete_if do |file| #iterate and delete if there are any ".." or "."
+    @files = Dir.entries(path) #put the list of files in the files directory!
+    @files.delete_if do |file| #iterate and delete if there are any ".." or "."
       file == ".." || file =="."
     end
   end
 
   def import
-     files.collect do |file_name|
+     @files.collect do |file_name|
         if !file_name.include?(Song.new_by_filename(file_name).artist.name)
           Song.new_by_filename(file_name)
         else
