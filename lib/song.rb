d02b25f982c25d450df82e5c912f4d5794265b4b
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-12 22:05:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
-    new_song.artist_name = artist
+    new_song.artist = artist
     new_song
   end
 
