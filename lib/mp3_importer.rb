56cd321879e89e06f1e4ee544ceb44790d9264d5
KerryAlsace <kerrydougherty@gmail.com>
2016-08-01 19:59:00 -0400

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,28 @@
+require 'pry'
+
 class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
 
   def files
+    files = []
+    Dir.entries(path).collect do |entry|
+      if entry.include?(".mp3")
+        files << entry
+      end
+    end
+    files
   end
 
   def import
+    self.files.each {|file_name| Song.new_by_filename(file_name)}
   end
 
-
-
 end
+
+test_music_path = "./spec/fixtures/mp3s"
+music_importer = MP3Importer.new(test_music_path)
+puts music_importer.files
--- lib/song.rb
+++ lib/song.rb
@@ -5,21 +5,17 @@ class Song
     @name = name
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def artist=(artist)
-    @artist = artist
-  end
-
   def self.new_by_filename(file_name)
-    self.artist = file_name.split(" - ")[0]
-    self.name = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    name = file_name.split(" - ")[1]
+    song = self.new(name)
+    song.artist_name = artist
+    song
   end
 
-  def self.artist_name
-    self.artist.name
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
 end
