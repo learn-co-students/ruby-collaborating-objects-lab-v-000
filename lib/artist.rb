d261f006407176b34b5841e08f666bcb797e1c6a
John Verdone <jmverdone@gmail.com>
2016-08-18 22:54:19 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,17 +5,14 @@ class Artist
     def self.all
       @@all
     end
-
-
-
     def initialize(name)
       @name = name
       @songs = []
+      save
     end
 
     def add_song(song)
       @songs << song
-      song.artist = self
     end
 
     def save
@@ -26,19 +23,11 @@ class Artist
       if self.all.find {|artist| artist.name == name}
           self.all.find {|artist| artist.name == name}
        else
-         Artist.new(name)
+          self.new(name)
       end
-    end
-
-    def self.find(name)
-
-
-
     end
 
     def print_songs
       songs.each {|song| puts song.name}
-
     end
-
   end
