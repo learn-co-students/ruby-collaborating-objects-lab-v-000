e6e0d22ae0119669061664533343b2319c56f4bb
Binny Lewis <binnylewis@gmail.com>
2016-08-19 22:57:33 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,16 +2,40 @@
 class Artist
   attr_accessor :name
   @@songs
+  @@all
 
   def initialize(name)
     @name = name
     @@songs = []
+    @@all = []
   end
 
   def self.all
     @@songs << self
   end
 
+  def save
+      @@all << self.name
+  end
 
+  def add_song(song)
+    @@songs << song
+  end
+
+  def songs
+    @@songs
+  end
+
+  def find_or_create_by_name(name)
+    if @all.include?(name)
+      name
+    else
+      self.new(name)
+    end
+  end
+
+  def print_songs
+    @@songs.each{ |song| puts song.name}
+  end
 
 end
