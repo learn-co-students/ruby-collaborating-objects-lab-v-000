f0a1db290aa0be208253e46f68fcc8ad79fbed1d
Binny Lewis <binnylewis@gmail.com>
2016-08-21 20:46:49 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,7 +14,7 @@ class MP3Importer
     #@@all = Dir.glob("#{@path}/**/*.mp3")
     #@@all.map{|file| File.basename(file)}
     #@path = Dir[@path].select { |e| e.split(/[\/]/).last }
-    #@@all = Dir[@path]
+    @@all = Dir[@path]
     #@path = @path.each{|file| file.split("/")}
     #@@all.map{|file| File.basename(file)}
     #@path = @path.each{ |file| "hello"}
