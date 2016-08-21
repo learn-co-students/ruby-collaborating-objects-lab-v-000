26a6a08be99bfeb6eb765cdc108b428130fcb96b
Binny Lewis <binnylewis@gmail.com>
2016-08-21 22:09:00 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 class MP3Importer
   attr_accessor :path
@@ -8,17 +9,22 @@ class MP3Importer
   end
 
   def files
-    @@all = Dir.entries(path).select { |file| file.end_with?(".mp3") }
+
+
+
+
     #@path = @path + "/*.mp3"
     #@@all = Dir.glob(@path)
     #@@all = Dir.glob("#{@path}/**/*.mp3")
-    #@@all.map{|file| File.basename(file)}
-    #@path = Dir[@path].select { |e| e.split(/[\/]/).last }
+    ##@@all.map{|file| File.basename(file)}
+    ##@@all = Dir[@path].select { |e| e.split(/[\/]/).last }
     #@@all = Dir[@path]
     #@path = @path.each{|file| file.split("/")}
     #@@all.map{|file| File.basename(file)}
     #@path = @path.each{ |file| "hello"}
     #/[\/]/.match(file)
+    @@all = Dir.entries(@path).select { |file| file =~ /.+\.mp3\Z/ }
+    #Dir.entries(path).select { |file| file.end_with?(".mp3") }
 
 
     #@@all_files.map { |file_path| File.basename(file_path) }
@@ -26,7 +32,8 @@ class MP3Importer
   end
 
   def import
-    @@all.each{ |file| Song.new_by_filename(file)}
+    binding.pry
+    @@all.each{ |file| binding.pry Song.new_by_filename(file)}
   end
 
 end
