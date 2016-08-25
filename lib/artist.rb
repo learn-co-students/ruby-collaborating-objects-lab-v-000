3e6f59f7492c2d81ac9fd89cbf683b0a735d172c
Malind Nikulski <malind.nikulski@gmail.com>
2016-08-25 22:25:55 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,4 +6,27 @@ class Artist
     @name = name
     @songs = []
   end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.find {|artist| artist.name == name}
+    else
+      self.new(name)
+    end
+  end
+
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
 end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.chdir(@path) do
+      @files = Dir.glob("*.mp3")
+    end
+    @files
+  end
+
+  def import
+    files.each {|song| Song.new_by_filename(song)}
+  end
+end
