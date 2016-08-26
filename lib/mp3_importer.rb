78274553155828b5d8d60a5a7124ea9c757438d8
Malind Nikulski <malind.nikulski@gmail.com>
2016-08-26 20:09:28 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,6 +13,6 @@ class MP3Importer
   end
 
   def import
-    files.each {|song| Song.new_by_filename(song)}
+    files.each {|file| Song.new_by_filename(file)}
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,14 @@
 class Song
   attr_accessor :name, :artist
 
-  def initialize(name = nil)
+  def initialize(name)
     @name = name
   end
 
   def self.new_by_filename(filename)
     song = self.new
-    song.name = filename.split(" - ")[1]
-    artist_name = filename.split(" - ")[0]
+    song_name = filename.split(" - ")[1]
+    song.artist_name = filename.split(" - ")[0]
     song.artist = Artist.find_or_create_by_name(name)
     song
   end
