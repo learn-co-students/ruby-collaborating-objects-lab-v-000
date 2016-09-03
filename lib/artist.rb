f73340e045349d1a2331296b0ee831f51ea77812
John Schlesinger <jschles1@ramapo.edu>
2016-09-03 04:26:40 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,6 +16,10 @@ class Artist
     @@all << self
   end
 
+  # This instance method exists to tell the artist about its songs. Simply take
+  # the Song instance that is passed in as an argument and store it in a songs
+  # array with all the other songs that belong to the artist.
+
   def add_song(song)
     @songs << song
   end
@@ -25,8 +29,14 @@ class Artist
   end
 
   def self.create(name)
-    self.new(name).tap {|artist| artist.save}
+    @name = name
+    @name.save
   end
+
+  # This class method should take the name that is passed in (remember, it will be a string),
+  # and do one of two things. Find the artist instance that has that name or create one
+  # if it doesn't exist. Either way, the return value of the method will be an instance
+  # of an artist with the name attribute filled out.
 
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.create(name)
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,9 +5,16 @@ class MP3Importer
     @path = path
   end
 
+  # Since we have to send the filenames to the Song class, we'll end up calling
+  # the following code in the #import method: Song.new_by_filename(some_filename).
+  # This will send us to the Song class, specifically Song.new_by_filename.
+
   def import
    files.each{|file| Song.new_by_filename(file)}
   end
+
+  # You should write code that responds to MP3Importer.new('./db/mp3s').import.
+  # Google around for how to get a list of files in a directory! Make sure you only get .mp3 files.
 
   def files
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |file| file.gsub("#{path}/", "") }
