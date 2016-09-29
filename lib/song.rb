bb2aebd76f22a41f8085a9cedbe1c18f79bd7d3e
vanita eckert <vanita112003@gmail.com>
2016-09-29 03:43:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,10 +9,10 @@ class Song
 
   def self.new_by_filename(file_name)
     song = self.new(name)
-    file_name.song = self
+    #  file_name.song = self
       new_file_name = file_name.split("-")
-      binding.pry
-      49-5
+      song.name = new_file_name[1]
+      # binding.pry
   end
 
     # describe '.new_by_filename' do
