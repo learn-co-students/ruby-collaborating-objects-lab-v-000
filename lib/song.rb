4e341304d7dee1c28271b826f07c98b0cf4e0d74
Nicholas Ratti <nickratti@gmail.com>
2016-11-08 01:58:03 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,25 +3,24 @@ class Song
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
-  def artist_name=(name)
+  def self.new_by_filename(file_name)
     #binding.pry
-    if self.artist = nil
+    song = self.new(file_name.split("-")[1].strip)
+    #song.artist = Artist.find_or_create_by_name(file_name.split("-")[0].strip)
+    artist_name=(file_name.split("-")[0].strip)
+    #binding.pry
+    song
+  end
+
+  def self.artist_name=(name)
+    if self.artist == nil
       self.artist = Artist.find_or_create_by_name(name)
       self.artist.songs = Artist.add_song(self.name)
     else
       self.artist.name
     end
-  end
-
-  def self.new_by_filename(file_name)
-    binding.pry
-    song = self.new(file_name.split("-")[1].strip)
-    song.artist = Artist.find_or_create_by_name(file_name.split("-")[0].strip)
-    song.artist_name=(file_name.split("-")[0].strip)
-    song
   end
 
 end
