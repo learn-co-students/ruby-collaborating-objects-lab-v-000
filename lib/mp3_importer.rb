9e610e5cee0642a8deb4f47797c3a569be65ef99
Senad Hadrovic <senadhadrovic@gmail.com>
2016-11-30 05:11:24 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,6 +11,5 @@ class MP3Importer
 
   def import
     files.each{|f| Song.new_by_filename(f)}
-    binding.pry
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,15 @@ end
 def self.new_by_filename(filename)
   artist_name = filename.split(" - ")[0]
   song_name = filename.split(" - ")[1]
-  artist = Artist.find_or_create_by_name(artist_name)
+  # artist = Artist.find_or_create_by_name(artist_name)
   song = self.new(song_name)
-  song.artist = artist
+  song.artist_name = artist_name
   song
 end
+
+def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
 end
