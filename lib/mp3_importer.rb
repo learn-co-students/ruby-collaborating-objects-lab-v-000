ba456857723680655bbe7ac03833a648375b432c
dwyn <dwayne.harmon@gmail.com>
2016-12-08 09:26:40 -0500

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,20 @@
+# require "../../spec/fixtures/mp3s"
+
 class MP3Importer
   attr_accessor :path
+  @@files = []
 
   def initialize(path)
     @path = path
+    # @@files = []
   end
 
   def files
+    @@files
   end
 
   def import
+    Dir["../spec/fixtures/mp3s"].each {|file| @@files << file }
+    # binding.pry
   end
 end
