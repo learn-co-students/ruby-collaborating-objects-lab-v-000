3d83ee05d93abbe7090d292ad7a2bd2b2c307255
dwyn <dwayne.harmon@gmail.com>
2016-12-15 20:39:19 -0500

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,23 +1,15 @@
-# require_relative '../spec/fixtures/mp3s'
-
 class MP3Importer
-  # extend Lovebot
-  attr_accessor :path
-  @@files = []
+  attr_reader :path
 
   def initialize(path)
     @path = path
   end
 
   def files
-
-    # binding.pry
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
   def import
-
-    # binding.pry
+    files.each{|f| Song.new_by_filename(f)}
   end
 end
-
-# Dir["../spec/fixtures/mp3s"].each {|file| @@files << file }
--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,13 @@ class Song
 
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
-    song_name = self.new(song)
-    song_name.artist = Artist.find_or_create_by_name(artist)
-    song_name
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 end
