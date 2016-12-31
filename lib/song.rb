476eccc995d22afc68e6f6ee48fc1795b54b30b1
Aniqa Elahi <aniqaelahi@yahoo.com>
2016-12-31 21:49:10 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,16 +7,17 @@ attr_accessor :name,:artist
   end
 
   def artist_name=(nam)
-    new_artist = Artist.find_or_create_by_name(nam)
-    new_artist.add_song(nam)
+#binding.pry
+    self.new_artist = Artist.find_or_create_by_name(nam)
+    new_artist.add_song(self)
     new_artist
 
 end
     def self.new_by_filename(filename)
       #binding.pry
-      artist,song = filename.split(" - ")
-      new_instance = self.new(song)
-      new_instance.artist_name=(artist)
+      #artist,song = filename.split(" - ")
+      new_instance = self.new(filename.split(" - ")[1])
+      new_instance.artist_name=filename.split(" - ")[1]
       new_instance
 
   end
