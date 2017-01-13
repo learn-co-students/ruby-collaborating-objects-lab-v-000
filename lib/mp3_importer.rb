439f87b4bc1805707717b883a16a232ac0cb535e
Scott Wakeman <swakeman@ca.rr.com>
2017-01-13 20:45:19 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,38 +4,21 @@ class MP3Importer
 
   def initialize(path)
     @path = path
-    test_music_path = "./spec/fixtures/mp3s"
-    music_importer = self.new(path)
-    #files =>Action Bronson - Larry Csonka - indie.mp3
+    self
   end
 
   def files
-    test_music_path = "./spec/fixtures/mp3s"
-    music_importer = MP3Importer.new(test_music_path)
-    #path = Dir.entries(test_music_path).import"
-
-    #path = MP3Importer.new('./db/mp3s').import
-    #Dir["/'./db/mp3s'/**/*.rb"]
-
-
-#files =>Action Bronson - Larry Csonka - indie.mp3
-     self.new(path)
-#expect(music_importer.files.size).to eq(4)
-
-  end
-
-
-#You should write code that responds to MP3Importer.new('./db/mp3s').import.
-#Google around for how to get a list of files in a directory! Make sure you only get .mp3 files.
-  def import
-    #files = Dir["/work/myfolder/**/*.txt"]
-    Find.find(@path) do |path|
-     if FileTest.directory?(path)
-       @dirs.push(path)
-     else
-       @files.push(path)
-     end
+    Dir::glob("./spec/fixtures/mp3s/*").collect do |f|
+      f.gsub("./spec/fixtures/mp3s/","")
     end
-    Song.new_by_filename(file_name)
   end
+
+  def import
+#write code that responds to MP3Importer.new('./db/mp3s').import
+    Song.new_by_filename(self.files[0])
+    binding.pry
+  end
+
+    #Song.new_by_filename(some_filename)
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class Song
-  attr_accessor :artist, :name
+  attr_accessor :artist, :name, :song
 
   def initialize(name)
     @name = name
@@ -24,10 +24,11 @@ class Song
    song                    #instance of Song
   end
 
-  def self.create_by_filname(file_name)
+  def self.create_by_filename(file_name)
     song = Song.new_by_filename(file_name)
     song.save
     song                    #instance of Song
+    binding.pry
   end
 
   def self.find_by_filename(file_name)
@@ -41,61 +42,8 @@ class Song
   def self.new_by_filename(file_name)
     file_name = file_name.split(" - ").first
     
-    #song = Song.new
-    
-    
-    #name = filename.split(".").first.split("- ").last  #perfrom multiple splits in one line
-    #filename.to_s.split(" - ").map do |filename|
-    #end  
-    #song.filename = file_name
+  
     song.file_name = file_name
     song
     end
-
-
-#It must parse a filename to find the song name and artist name. 
-#(Hint: every file separates the song and artist with a " - ") 
-    #filename.to_s.split(" - ").map do |filename|
-    
-#create a new song instance using the string we gathered from the filename.
-    #song = new_by_filename(filename)
-    #song.save
-    #song
-    #song = self.new
-   #song.title = filename.split(" - ")[1]
-   #song
-
-#want to associate that new song with an artist. 
-#To do this we'll use a helper method: Song#artist=(artist_name)
-
-  #artist=(artist_name)
-
-#Return the new song instance.
-
-#our_song_instance.artist = our_artist_instance
-# Since we only have the artist name as a string (not an instance of the Artist class), 
-#we'll create a method that takes in the name and gets the artist object. 
-#Let's call this Song#artist=(artist_name). 
-  #artist=(artist_name)
-  #artist_name = filename.split(" -").first
-  #name = filename.split(".").first.split("- ").last  #perfrom multiple splits in one line
-  #song.name = name
-  #song.artist_name = artist_name
-  #artist_name
-
-  
-
 end
-
-    
-#Turn the artist's name as a string into an artist object
-  #Either we have to create that artist instance
-  #Or it already exists and we have to find that artist instance.
-
-  #Artist.find_or_create_by_name(artists-name-here)
-
-  #end
-
-  #def add_song(some_song)
-
-  #end
