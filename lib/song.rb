75c5804c9aa43b9cc6f79bad6394bd3bffb3f8fc
github username <github email address>
2017-01-13 16:32:31 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+
+  end
+
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+  # def artist=(name)
+  #   if (self.artist.nil?)
+  #     self.artist = Artist.new(name)
+  #   else
+  #     self.artist.name = name
+  #   end
+  # end
+end
