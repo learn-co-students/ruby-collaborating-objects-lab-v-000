024ba08d4b65dfd40aa16ef183d2777bbe680482
charlie lee <ctlee9096@gmail.com>
2017-02-28 05:15:40 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,52 @@
 require 'pry'
 class Artist
   attr_accessor :name, :songs
-    @@all = []
-  def initialize(name)
-    @name = name
-    @songs = []
-    #@artist = artist
-  end
-    def add_song(song)
-      @songs<< song
-    end
-    def self.all
-      @@all
-    end
-    def save
-      @@all << self
-    end
-  def self.find_or_create_by_name (name)
-    if self.all.detect {|i| i.name == name}
-      self.all.detect {|i| i.name == name}
-    else
-      self.create(name)
-    end
-  end
-  def self.create(name)
-      artist = self.new(name)
-      binding.pry
-      artist.save
-      binding.pry
-      artist
-  end
-  def print_songs
-    songs.each{|song| puts song.name}
+  #each artist's name is name
+    #@songs = []
+  @@all =[]
+  def name #getter method cRud
+    @name
     #binding.pry
   end
+  def name=(name) #setter method, Crud
+    @name=name
+  end
+  def songs #getter method cRud
+    @songs
+    #binding.pry
+  end
+  def songs=(songs) #setter method, Crud
+    @songs=songs
+  end
+  def initialize(name)
+    @name = name
+    @songs = []#Crud-array to track songs when new artist is
+    #instantiated
+    #binding.pry
+  end
+  def add_song(song)
+    #binding.pry
+    @songs.push song
+    #binding.pry
+  end
+  def save
+    #binding.pry
+    @@all << self#adds class artist instance
+  end
+  def self.all
+    #binding.pry
+    @@all
+    #binding.pry
+  end
+  def self.find_or_create_by_name(name)
+    #binding.pry
+    if self.all.find{|artist| artist.name == name}
+    #binding.pry
+      self.all.find{|list_item| list_item.name == name}
+    else
+      new_artist = Artist.new(name)
+    #binding.pry
+    end
+  end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,13 @@
+require 'pry'
 class Song
-  attr_accessor :name, :artist
-
+  def name #getter method cRud
+    @name
+  end
+  def name=(name) #setter method, Crud
+    @name=name
+  end
   def initialize(name)
     @name = name
-  end
-
-  def self.new_by_filename(filename)
-    split = filename.split(" - ")
-    artist_name = split[0]
-    song_name = split[1]
-    song = self.new(song_name)
-    song.artist_name = artist_name
-    song
-  end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+    #binding.pry
   end
 end
