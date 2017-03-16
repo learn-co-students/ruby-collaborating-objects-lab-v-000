4705239a1bb5bb9348c9f27d445841a49ab871c6
Jessica Widman <jawidman.30@gmail.com>
2017-03-16 00:27:45 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class MP3Importer
+  attr_accessor :path, :filenames
+
+  def initialize(path)
+    @path = path
+    @filename_lib = []
+  end
+
+  def files
+    @filenames = Dir.entries(@path).delete_if{|w| w.size<3}
+  end
+
+  def import
+    self.files
+    @filenames.each do |filename|
+      song = Song.new_by_filename(filename)
+      @filename_lib << song
+    end
+  end
+
+end
