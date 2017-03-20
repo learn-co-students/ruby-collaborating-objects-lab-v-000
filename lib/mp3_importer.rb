ea66f7ea47cb0e0b519bfd317d1f49240b0fa456
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-03-20 07:24:18 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.glob("#{@path}/*.mp3").select{|f| File.file? f}.collect{ |f| File.basename f}
+  end
+
+  def import
+    self.files.each{|filename| Song.new_by_filename(filename)}
+  end
+
+end
