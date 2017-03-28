b6d4e8bbe34cf8086763b026a3aafa9bc5547e8a
Eric Levy <levyeric10@gmail.com>
2017-03-28 22:22:29 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,24 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
+  @@all = []
 
   def initialize(name)
     @songs = []
     @name = name
   end
 
-  def add_song
-    
+  def add_song(songs)
+    @songs << songs
   end
-  
+
+  def self.all
+    @@all
+  end
+
+  def save
+  @@all << self
+  end
+
 end
