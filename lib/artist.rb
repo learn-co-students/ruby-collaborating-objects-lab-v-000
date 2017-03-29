ce34d6e13ab8a2e4510b3f40d3ebf7bfa0d3b7c1
jamcelr <jamcelr@hotmail.com>
2017-03-28 22:20:29 -0400

    Updated and refactored code for all classes in /lib

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,29 +4,68 @@ class Artist
   @@all = []
 
   def initialize(name)
-    self.name = name
-    self.songs = []
+    @name = name
+    @songs = []
     save
-  end
-
-  def add_song(song)
-    self.songs << song
-  end
-
-  def save
-    @@all << self
   end
 
   def self.all
     @@all
   end
 
+  def self.find_by_name(name)
+    self.all.detect{|artist| artist.name == name}
+  end
+
   def self.find_or_create_by_name(name)
-    @@all.detect {|artist| artist.name == name} || Artist.new(name)
+    self.find_by_name(name) || self.new(name)
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
   end
 
   def print_songs
-    self.songs.each {|song| puts song.name}
+    @songs.each {|song| puts song.name}
   end
-
 end
+
+
+# Previous code
+
+# class Artist
+#   attr_accessor :name, :songs
+
+#   @@all = []
+
+#   def initialize(name)
+#     self.name = name
+#     self.songs = []
+#     save
+#   end
+
+#   def add_song(song)
+#     self.songs << song
+#   end
+
+#   def save
+#     @@all << self
+#   end
+
+#   def self.all
+#     @@all
+#   end
+
+#   def self.find_or_create_by_name(name)
+#     @@all.detect {|artist| artist.name == name} || Artist.new(name)
+#   end
+
+#   def print_songs
+#     self.songs.each {|song| puts song.name}
+#   end
+
+# end
--- lib/song.rb
+++ lib/song.rb
@@ -2,18 +2,41 @@ class Song
   attr_accessor :name, :artist
 
   def initialize(name)
-    self.name = name
+    @name = name
   end
 
   def self.new_by_filename(filename)
-    info = filename.split(" - ")
-    song = self.new(info[1].gsub('.mp3',''))
-    song.artist_name = info[0]
+    data = filename.split(/\s\-\s|\./)
+    song = self.new(data[1])
+    song.artist_name = data[0]
     song
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
-
 end
+
+
+# Previous code
+
+# class Song
+#   attr_accessor :name, :artist
+
+#   def initialize(name)
+#     self.name = name
+#   end
+
+#   def self.new_by_filename(filename)
+#     info = filename.split(" - ")
+#     song = self.new(info[1].gsub('.mp3',''))
+#     song.artist_name = info[0]
+#     song
+#   end
+
+#   def artist_name=(name)
+#     self.artist = Artist.find_or_create_by_name(name)
+#   end
+
+# end
