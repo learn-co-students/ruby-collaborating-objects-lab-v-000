223c7e87cc8780ea6db2a59742a73ae6a7dfad7d
Sara Hopper <sara.hopper@gmail.com>
2017-04-01 14:01:14 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = []
+      Dir.foreach(@path) do |filename|
+      files << filename if filename.include?".mp3"
+    end
+    files
+  end
+
+  def import
+    files.each { |filename| Song.new_by_filename(filename) }
+  end
+
+end
