a2248864b47de80984c11a2938f37a680da2f12f
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-04-09 23:34:45 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+ require 'pry'
+
+  class MP3Importer
+    attr_accessor :path
+
+    def initialize(path)
+      @path = path
+    end
+
+    def files
+      @mp3_files = Dir.glob(File.join(@path, "*.mp3"))
+      @mp3_files.map { |f| File.basename(f) }
+    end
+
+    def import
+      files.each { |filename| Song.new_by_filename(filename) }
+    end
+
+
+  end
