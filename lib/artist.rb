39320eb82c2f7f69a6f9d33cff5fa2a158dd1c72
sam oteo <tortillabar@me.com>
2017-04-12 21:45:57 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+#create the artist(if artist doesnt exist in our program yet)
+#finding the instance of that artist(if artist does exist)
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+    self.all.detect { |n| n.name == name } || self.new(name)
+  end
+
+  def print_songs
+    i = 0
+    while i < self.songs.length
+      puts self.songs[i].name
+      i += 1
+    end
+  end
+end
