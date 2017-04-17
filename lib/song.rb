c84bd5475f05c011970ac2ed32153066185d1962
David Mars <david.w.mars@my.occc.edu>
2017-04-17 02:43:34 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+#   ~~~HAVICK WAS HERE~~~
+
+class Song
+  attr_accessor :name, :artist
+@@all = []
+  def initialize(name)
+@name = name
+  end
+  
+  def self.new_by_filename(filename)
+      artist, song = filename.split(" - ")
+      new_song = Song.new(song)
+      new_song.artist_name = artist
+      new_song.save
+    end
+
+def artist_name=(name)
+  self.artist = Artist.find_or_create_by_name(name)   
+  artist.add_song(self) 
+end
+def save
+  @@all << self
+  self
+end
+end
