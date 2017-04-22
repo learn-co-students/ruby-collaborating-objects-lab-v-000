d7e6997a58803cfed9f2bdb7a575d1564904071f
AJ <ajoeford@gmail.com>
2017-04-21 18:32:57 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,16 @@ class Song
     self.name = name
   end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
+
   def self.new_by_filename(filename)
 
     array = filename.split("-")
     new_song = Song.new(array[1].strip)
-    new_song.artist = Artist.find_or_create_by_name(array[0].strip)
-    new_song.artist.add_song(new_song)
+    new_song.artist_name = array[0].strip
     new_song
   end
 
