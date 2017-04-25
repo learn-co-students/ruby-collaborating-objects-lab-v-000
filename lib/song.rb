72a691d65c835bacb7ae5f5988668bc010c26d83
Julie Widman <juliewdmn@gmail.com>
2017-04-25 11:29:43 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  # def name=(name)
+  #   @name = name
+  # end
+
+  # def artist=(artist)
+  #   @artist = artist
+  # end
+end
