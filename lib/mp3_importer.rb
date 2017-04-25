8ffa3b9cb4c58155b30755df1a6f29c062956f14
Julie Widman <juliewdmn@gmail.com>
2017-04-25 17:15:41 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,30 +3,17 @@ require 'pry'
 class MP3Importer
   attr_accessor :path
 
-  @@music_importer = []
-
   def initialize(path)
     @path = path
   end
 
   def files
-    @@music_importer << Dir.glob("./spec/fixtures/*.mp3").each{|file| load file}
-      @@music_importer
-    #yield files.each{|file|
-    #helper methods needed here #=> normalize_filename
-    #should I put a yield? ex: yield #=> @@music_importer << file
-    #binding.pry
-    #.each {|file| require file }
-    #loads mp3 files in the path directory
-    #Dir.glob path/*.mp3
-    #gsub
+     Dir.glob("./#{path}/*.mp3").collect{|file| file.gsub("./#{path}/", '')}
   end
 
+  def import
+    files.each do |file|
+      Song.new_by_filename(file)
+    end
+  end
 end
-
-
-# dir = "/path/to/directory"
-# $LOAD_PATH.unshift(dir)
-# Dir[File.join(dir, "*.rb")].each {|file| require File.basename(file) }
-#^^^if just loading the contents of the file and don't need file more than once
-#perhaps load should be used instead of require (don't like global variable here)
--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,29 @@
 class Song
-
-  attr_accessor :name, :artist
-
+  attr_accessor :name, :artist, :mp3_importer
+  @@all = []
   def initialize(name)
     @name = name
   end
 
-  # def name=(name)
-  #   @name = name
-  # end
+  def self.new_by_name(name)
+    song = self.new(name)
+    song
+  end
 
-  # def artist=(artist)
-  #   @artist = artist
-  # end
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    @@all << song
+    song
+  end
+
+  def self.new_by_filename(file)
+    file = file.split(" - ")
+    artist = file[0]
+    song = file[1].gsub(".mp3", "")
+       #binding.pry
+    new_song = self.create_by_name(song)
+    new_song.artist = Artist.find_or_create_by_name(artist)
+    new_song.artist.add_song(new_song)
+    new_song
+  end
 end
