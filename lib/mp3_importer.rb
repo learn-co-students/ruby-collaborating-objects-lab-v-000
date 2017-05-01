435cbeaa42771fa946aa967dc941b2373e85820d
Will-Sauer <will@heueso.com>
2017-04-30 18:55:20 -0700

    mp3 #init and #files working but not tested

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+class MP3Importer
+attr_accessor :path, :files, :normalized_files
+
+#Dir["/path/to/search/**/*.rb"]
+
+def initialize (path)
+@path = path
+@files = []
+end
+
+
+def files
+@files = Dir["#{path}/**/*.mp3"]
+@normalized_files = []
+@files.each {|filename| @normalized_files << filename.sub(Regexp.new("#{path}/"), "")}
+@normalized_files
+end
+
+
+
+end
