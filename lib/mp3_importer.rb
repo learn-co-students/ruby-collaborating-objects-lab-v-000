bd1aadae46e8702cf159a79be5472b599a264fc6
Elysa Silveira <elysa.silveira@gmail.com>
2017-05-31 16:44:52 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = Dir.glob("#{path}/*.mp3")
+    files.map do |f|
+      f.slice!(21..-1)
+    end
+
+  end
+
+  def import
+    files.each{ |filename| Song.new_by_filename(filename) }
+  end
+
+end
