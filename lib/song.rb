200f00b8a1a043313bd03e764c34e5a2b64d34e2
ctab33 <ctab33@me.com>
2017-06-02 15:28:02 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end 
+
+end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 describe 'Artist' do
   let(:artist) {Artist.new('Michael Jackson')}
 
@@ -27,9 +29,11 @@ describe 'Artist' do
   describe '#save' do
     it 'adds the artist instance to the @@all class variable' do
       artist.save
+    
       expect(Artist.all).to include(artist)
     end
   end
+
 
   describe '.find_or_create_by_name' do
     it 'finds or creates an artist by name maintaining uniqueness of objects by name property' do
