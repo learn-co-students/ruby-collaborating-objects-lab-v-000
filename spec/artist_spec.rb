eed4e9154acd4ca23bc9a5789f18ea5e27b8b32d
zoebisch <zoebisch@gmail.com>
2017-06-01 19:30:33 -0700

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 describe 'Artist' do
   let(:artist) {Artist.new('Michael Jackson')}
 
@@ -35,6 +36,7 @@ describe 'Artist' do
     it 'finds or creates an artist by name maintaining uniqueness of objects by name property' do
       artist_1 = Artist.find_or_create_by_name("Michael Jackson")
       artist_2 = Artist.find_or_create_by_name("Michael Jackson")
+
       expect(artist_1).to eq(artist_2)
     end
 
