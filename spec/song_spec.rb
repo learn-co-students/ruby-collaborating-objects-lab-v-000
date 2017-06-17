834f08115ce5d5ae208ff810578f7a2e77b76472
Shaun McLoughlin <skmcloughlin@gmail.com>
2017-06-17 06:27:05 -0400

    Update song_spec.rb

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -27,7 +27,7 @@ describe 'Song' do
     it 'creates a new instance of a song from the file that\'s passed in' do
       new_instance = Song.new_by_filename(file_name)
       expect(new_instance.name).to eq('Black or White')
-      #expect(new_instance.artist.name).to eq('Michael Jackson')
+      expect(new_instance.artist.name).to eq('Michael Jackson')
     end
   end
 end
