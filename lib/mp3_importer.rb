4087c6a849ec035e0d8f862e83aa6516e18f99fc
Jacqlene Sifre <jsifre01@gmail.com>
2017-06-22 23:58:13 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,14 +1,24 @@
 class MP3Importer
 
-  def initialize(file_path)
+  attr_accessor :path
 
+  @@songs = []
+
+  def initialize(path)
+    @path = path
   end
 
   def files
-
+    files = Dir["#{path}/*.mp3"]
+    files.collect do |file|
+      file.split("mp3s/")[1]
+    end
   end
 
   def import
-
+    files.each do |file|
+      @@songs << Song.new_by_filename(file)
+    end
+    @@songs
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,17 @@
 class Song
-
-  attr_accessor :name, :artist
+  attr_accessor :artist, :name
 
   def initialize(name)
     @name = name
   end
 
-  def self.new_by_filename
-
+  def self.new_by_filename(filename)
+    full_name = filename.split(" - ")
+    artist = Artist.find_or_create_by_name(full_name[0])
+    name = full_name[1]
+    song = self.new(name)
+    artist.add_song(song)
+    song.artist = artist
+    song
   end
-
 end
