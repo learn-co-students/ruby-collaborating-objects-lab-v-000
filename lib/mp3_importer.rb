835e7c8aa27714aeeb88fb96e17091631ef04fbe
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-06-28 21:32:24 +0000

    done

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{|file| file.gsub("#{path}/", "")}
+  end
+
+  def import
+    files.each do |file_name|
+      Song.new_by_filename(file_name)
+    end
+  end
+end
