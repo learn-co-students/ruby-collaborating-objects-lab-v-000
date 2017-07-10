f577350ca32cfb3faa23825a8f5b9bb837477c10
Scyntre <github email address>
2017-07-10 06:16:49 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,13 @@ class Song
 
   def initialize(name, artist = nil)
     self.name = name
-    self.artist = artist
+    self.artist = Artist.find_or_create_by_name(artist)
   end
 
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1].gsub(".mp3", ""), filename.split(" - ")[0])
-    Artist.find_or_create_by_name(song.artist).add_song(song.name)
+    song.artist.add_song(song.name)
+    song
   end
 
 end
