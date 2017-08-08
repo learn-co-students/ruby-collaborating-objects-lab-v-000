1e891f602633a71df8256f019725cead6f19c076
churchwerks <steven.lennis@icloud.com>
2017-08-08 15:33:03 -0500

    Artist tests passing

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,4 +19,22 @@ class Artist
   def save
     @@all << self
   end
+
+  def self.create_by_name(artist_name)
+    self.new(artist_name)
+  end
+
+  def self.find_by_name(artist_name)
+    self.all.detect {|item| item.name == artist_name}
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    self.find_by_name(artist_name) || self.create_by_name(artist_name)
+  end
+
+  def print_songs
+    self.songs.each { |name| puts "#{name.artist}" }
+  end
+
+
 end
