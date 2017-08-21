f7b54098d4226ddb1da678acffae45242ce90a49
Carley Tripp <carley.tripp@outlook.com>
2017-08-21 00:04:18 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class MP3Importer
+  attr_accessor :path, :files
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.entries("#{@path}").grep(/mp3/)
+  end
+
+  def import
+    files.each {|file| Song.new_by_filename(file)}
+  end
+end
