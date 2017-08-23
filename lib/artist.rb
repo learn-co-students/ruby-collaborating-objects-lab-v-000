4f5d3e3dc49a6951f2d3da026f20e8181943eb33
Jennifer Nelson <jennifernelson118@gmail.com>
2017-08-23 00:17:18 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,5 @@
+
+
 class Artist
   attr_accessor :name, :songs
   @@all = []
@@ -9,20 +11,32 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    save
   end
 
   def save
     @@all << self
   end
 
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
   def self.find_or_create_by_name(name)
-    @@all.find do |artist|
-      if artist.name == name
-      else
-        self.new(name)
-      end
+    result = @@all.find {|artist| artist.name == name}
+    if result
+      result
+    else
+      self.create(name)
     end
+
+    # @@all.find do |artist|
+    #   if artist.name == name
+    #   else
+    #     self.new(name)
+    #   end
+    # end
   end
 
   def add_song(song)
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class MP3Importer
   attr_accessor :path
 
