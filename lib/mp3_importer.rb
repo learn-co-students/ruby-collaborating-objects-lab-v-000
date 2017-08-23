d9a4c311a5f9d3fbe55cd3b0e4a2e6bad17cdec4
github username <github email address>
2017-08-22 22:23:48 -0700

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -12,10 +12,13 @@ class MP3Importer
   def import
     self.files.each do |file|
       Song.new_by_filename(file)
+      binding.pry
     end
   end
 
 end
+
+
 
 # MP3Importer.new('./db/mp3s').import
 
--- lib/song.rb
+++ lib/song.rb
@@ -8,18 +8,19 @@ class Song
 
   def self.new_by_filename(file_name)
     normalized = file_name.split(" - ")
-    artist_name = normalized[0]
-    song_title = normalized[1]
-    song = self.new(song_title)
+    the_artist_name = normalized[0]
+    the_song_title = normalized[1]
+    song = self.new(the_song_title)
     # binding.pry
     # song.artist = artist_name(artist_name)
-    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.artist_name = the_artist_name
     song
   end
 
-  # def artist_name(artist_name)
-  #   Artist.find_or_create_by_name(artist_name)
-  # end
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(self)
+  end
 
 end
 
