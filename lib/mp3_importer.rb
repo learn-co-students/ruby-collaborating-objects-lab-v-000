45c9be6ba4ee6fd8f1ec7bf08797f3a5ce7a7c14
Susan Fowler <infopro94@gmail.com>
2017-09-02 02:53:23 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,24 +1,26 @@
+require 'pry'
+
 class MP3Importer
 
   attr_accessor :path, :files
   @path = []
 
-  def initialize(path)
+  def initialize(path) #accepts file path to parse mp3 files
     @path = path
   end
 
-  def files
-      path = Dir["/./.mp3"]
-      path.each do |file_name|
-      file_name.gsub("/", "-")
-
-
-    end
+  def files #loads all mp3 files in the path directory
+    #normalizes the filename to just the mp3 filename w/no path
+      Dir['path/to/dir/*.mp3'].select { |e| File.file?(e) }
+      # files = Dir["/./.mp3"]
+      # binding.pry
+      # @path<<files
+    # end
       # file_name = "/db/mp3s"
    end
 
-  def import(file_name)
-    import = Song.new_by_filename(file_name)
+  def import(file_name) #imports the files into the library by creating songs from a filename
+      import = Song.new_by_filename(file_name)
   end
 
 end
