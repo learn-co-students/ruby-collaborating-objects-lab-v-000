94b3af8a88f0b986d1b3c58b3b23629c26343525
Hunter Kleindl <hkleindl@gmail.com>
2017-09-10 23:21:23 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,8 +14,7 @@ class MP3Importer
   end
 
   def import
-    files.each {|file_name|
-    Song.new_by_filename(file_name)}
+    files.each {|file_name| Song.new_by_filename(file_name)}
   end
 
 end
