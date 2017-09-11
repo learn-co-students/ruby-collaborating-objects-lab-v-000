9dbde58f33d0d626f2970015af690765c9115ecc
Jessie Huff <huffjd1@gmail.com>
2017-09-11 16:06:01 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,33 @@
 class Artist
   attr_accessor :name, :songs
 
-  @@all = []
+  @@all = []  #class variable to store artist instances
 
   def initialize(name)
     @name = name
     @songs = []
-    save
+    @@all << self
   end
 
   def add_song(song)
     @songs << songs
-  end
+  end  #adds new song to the instance variable @songs
 
-  def save
-    @@all << self
-  end
+#  def save
+#    @@all << self
+#  end #adds artist instance to the @@all class variable
 
   def self.all
     @@all
-  end
+  end #class reader to access all instances of an artist
 
   def self.find_or_create_by_name(name)
     unless @@all.detect {|artist| artist.name == name}
       name = Artist.new(name)
     end
-  end
+  end #class finder to detect existing artist. Creates new instance if not found.
 
   def print_songs
     self.songs.each {|song| puts song.name}
-  end
+  end #prints list of all artist's songs.
 end
