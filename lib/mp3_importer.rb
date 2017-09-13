270f2d053a4b9980a0feb921f7145e5ae4b6cde5
Robin Pahlman <robin.pahlman@gmail.com>
2017-09-13 00:10:57 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = Dir.entries(@path).grep(/.mp3/)
+  end
+
+  def import
+    files.each {|file_name| Song.new_by_filename(file_name)}
+  end
+
+end
