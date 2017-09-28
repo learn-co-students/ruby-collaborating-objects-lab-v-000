bff1031f86407f1af447ab66e38328e315966e86
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-09-28 00:47:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -8,12 +8,13 @@ class Song
   def self.new_by_filename(file_name)
     file_split = file_name.split(" - ")
     new_instance = self.new(file_split[1])
-    new_instance.artist_name=(file_split[0])
+    new_instance.artist_name = file_split[0]
   end
 
   def artist_name=(name)
     new_instance = Artist.find_or_create_by_name(name)
     new_instance.add_song(self)
+    self.artist = new_instance
     new_instance
   end
 end
