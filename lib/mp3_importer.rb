77ceec8f4d255209fc8e6076e1dff9d1c11db95a
Tony Distinti <adistinti@gmail.com>
2017-09-29 15:02:13 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,6 +9,7 @@ class MP3Importer
   end
 
   def files
+    #The Dir.glob lists the files in path, and the .collect...gsub returns an array with just the filenames, not the junk indicating the path.
     Dir.glob("#{path}/*.mp3").collect { |x| x.gsub(path + "/", "")}
   end
 
