e32166698db09175c1fb1fed13b7646b2989fcff
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-30 19:54:16 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist
+  attr_accessor :name
+  @songs = []
+  @@all = []
+  def initialize(name)
+    @name = name
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def add_song(song)
+    @songs << song
+    #@@song_count += 1
+    song.artist = self
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.any? { |artist| artist.name == name }
+      return @@all.find { |artist| artist.name == name}
+    else
+      self.new(name)
+    end
+  end
+
+  def songs
+  end
+
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
+end
