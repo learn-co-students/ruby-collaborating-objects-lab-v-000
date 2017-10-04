2b173f65d68bbfe42bae0a71dbe8523fe7e5a3b4
Adam Tropp <adt6261@gmail.com>
2017-10-04 01:22:55 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -8,11 +8,12 @@ class Song
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
+
+  def artist_name
+    @artist
   end
 
   def self.new_by_filename(filename)
