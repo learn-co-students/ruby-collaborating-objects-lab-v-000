c1db6d0b44dd189bf4c5d3d05eed239e24a2b662
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-15 10:35:55 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,28 @@
 class Artist
 attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     self.name = name
     @songs = []
   end
 
-  def songs(song_name)
+  def songs
+    @songs
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song_name)
     @songs << song_name
   end
 
+  def save
+    @@all << self
 
+  end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 class Song
   attr_accessor :title, :artist
 
+  def initialize(title)
+    self.title = title
+  end
+
   def self.new_by_filename(filename)
     song = self.new
     song.artist = filename.split(" - ")[0]
