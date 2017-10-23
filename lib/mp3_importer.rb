e5e7f4e12b15bc4e85a023b4a248c583154b026b
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-23 15:27:09 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,20 +1,39 @@
-require 'pry'
+# require 'pry'
+# class MP3Importer
+#   attr_accessor :path
+#
+#   def initialize(path)
+#     @path = path
+#
+#   end
+#
+#   def files
+#     @files ||= Dir.glob("#{path}/*.mp3").map {|e| e.gsub("#{path}/", "")}
+#     # or/equals :: if left side is true, don't check the right.
+#     #Dir.glob -- Dir== directory streams, lists directories and paths and contents
+#     # glob == not a regex * matches any file, ** matches directories recursively,
+#   end
+#
+#   def import
+#     binding.pry
+#     files.each do|f|
+#       Song.new_by_filename(f)
+#       Artist.find_or_create_by_name(f)
+#     end
+#   end
+# end
 class MP3Importer
-  attr_accessor :path
+  attr_reader :path
 
   def initialize(path)
     @path = path
-
   end
 
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").map {|e| e.gsub("#{path}/", "")}
-    # or/equals :: if left side is true, don't check the right.
-    #Dir.glob -- Dir== directory streams, lists directories and paths and contents
-    # glob == not a regex * matches any file, ** matches directories recursively,
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
   def import
-    @files.each{|filename| Song.new_by_filename(filename)}
+    files.map{|f| Song.new_by_filename(f)}
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,22 @@
+require 'pry'
 class Song
-  attr_accessor :artist
+  attr_accessor :artist, :name
+  @@all = []
 
-  def initialize(name)
-    song = Song(name)
+  def initialize(name, artist)
+    @name = name
   end
 
-  def new_by_filename(filename)
-    song = Song("filename")
+  def self.new_by_filename(filename) #figure out what the filename format is, and use split or gsub.
+    # binding.pry
+    parts = filename.split(" - ")
+    new_song = self.new(parts[1])
+    new_song.artist_name= parts[0]
+    new_song
   end
-  
+  #
+  # def artist_name=(name)
+  #    self.artist= Artist.find_or_create_by_name(name)
+  #    artist.add_song(self)
+  # end
 end
