4e62cd40e9a3752a476b931ddbf624cca0a3e4f7
Anastasia  Barkett <anastasia.barkett@gmail.com>
2017-10-26 22:40:21 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,8 +31,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
-    #self.find(name) || self.create(name)
+    self.find(name) || self.create(name)
   end
 
   def print_songs
