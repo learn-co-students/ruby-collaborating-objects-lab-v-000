93c65d0d59ac25cc4210a570d97bdb090d84cda6
github username <wigmore3.14@gmail.com>
2017-10-28 16:43:41 -0700

    Collaborating objects - Done

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+  attr_reader :path
+
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files = Dir.glob("#{path}/*.mp3").collect { |file| file.gsub("#{path}/", "") }
+
+  end
+
+  def import
+    files.collect { |file| Song.new_by_filename(file) }
+  end
+
+end
