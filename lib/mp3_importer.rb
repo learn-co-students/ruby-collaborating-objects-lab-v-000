5fa91035aac04e71b818012bde286ecb7b37388c
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-30 17:32:45 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,9 +3,14 @@ class MP3Importer
   def initialize(path)
     @path = path
   end
-  def import
-
+  def files
+    Dir[path+"/**/*.mp3"].collect{|e| e.split("/").last}
   end
-
-
+  def import
+    Artist.clear_list
+    #binding.pry
+    self.files.each{|e|
+      Song.new_by_filename(e)
+    }
+  end
 end
---
+++ lib/test.rb
@@ -0,0 +1,6 @@
+require_relative "artist.rb"
+require_relative "song.rb"
+require_relative "mp3_importer.rb"
+
+song = Song.new("Free")
+puts song
