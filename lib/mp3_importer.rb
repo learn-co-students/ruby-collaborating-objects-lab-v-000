54f955eb777d8fb07c2abfff323db3e57a893ed4
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-01 21:09:53 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,23 @@
+class MP3Importer
+
+  attr_accessor :path, :files
+
+  def initialize(file_path)
+    @path = file_path
+  end
+
+  def files
+    @files = Dir.foreach(path).select do |x|
+      File.file?("#{path}/#{x}")
+    end
+  end
+
+  def import
+    self.files
+    if @files
+      @files.each do |file|
+        Song.new_by_filename(file)
+      end
+    end
+  end
+end
