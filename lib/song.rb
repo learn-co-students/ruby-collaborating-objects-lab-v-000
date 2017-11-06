6a53dd4138b815daa6acf680144986119053823c
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-06 20:13:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,10 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    self.new(filename.split(" - ")[1])
+    song = self.new(filename.split(" - ")[1])
+    song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
+    song.artist.add_song(song)
+    song
   end
-  
+
 end
