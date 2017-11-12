ac4eb215b5c44b98b3d729e434fd79a1ae5a2024
Yosa Yon <yosa.yon@gmail.com>
2017-11-12 19:02:00 +0000

    added notes to better conceptualize the relationship between the classes

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,26 +11,30 @@ class Artist
     @@all
   end
 
+  def songs
+    @songs
+  end
+
   def save
     self.class.all << self
   end
 
-  def add_song(song)
-    self.songs << song
-  end
-
   def self.find_or_create_by_name(name)
-    self.find(name)? self.find(name) : self.create(name)
+    self.find(name) ? self.find(name) : self.create(name)
   end
 
   def self.find(name)
-    self.all.find{|artist| artist.name == name}
+    self.all.find{|artist|artist.name == name}
   end
 
   def self.create(name)
-    artist = self.new(name)
+    artist = Artist.new(name)
     artist.save
     artist
+  end
+
+  def add_song(song)
+    self.songs << song
   end
 
   def print_songs
--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,43 @@
 class Song
-  attr_accessor :artist, :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
 
   def self.new_by_filename(filename)
-    record = filename.split(" - ")
-    song = self.new(record[1])
-    song.artist_name = record[0]
-    song
+    artist,song = filename.split(" - ")
+    new_song = self.new(song)
+    #below code is where we send the artist string to the
+    #artist_name=(name) method
+    new_song.artist_name = artist
+    new_song
   end
 
   def artist_name=(name)
+    #below code is where you assign the song an artist object.
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
+
 end
+#new_song = self.new(song)
+#crazy_in_love = Song.new("Crazy in Love")
+
+#new_song.artist_name = artist
+#crazy_in_love.artist_name("beyonce")
+
+#crazy_in_love.artist = Artist.find_or_create_by_name("Beyonce")
+#Artist.find("Beyonce") || Artist.create("Beyonce")
+
+#If Artist.find("Beyonce"), @@all.find{|artist|artist.name == "Beyonce"}
+#returns artist object with name "Beyonce"
+#Then add the song to the Beyonce's repertoire
+#aka beyonce.add_song(crazy_in_love)
+#beyonce.songs => ["Crazy in Love"]
+
+#If Artist.create("Beyonce"), artist = Artist.new("Beyonce")
+#artist.save or @@all << artist
+#return the artist object
+#Then add the song to Beyonce's repertoire
+#aka artist.add_song(crazy_in_love)
