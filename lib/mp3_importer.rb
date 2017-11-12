18b82f7a99616940ed354ac20c1a3662d803b0ea
Miwa Hirano <paintfx@gmail.com>
2017-11-12 00:41:31 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").map{ |file| file.gsub("#{path}/", "") }
+  end
+
+  def import
+    self.files.each{ |filename| Song.new_by_filename(filename) }
+  end
+
+end
