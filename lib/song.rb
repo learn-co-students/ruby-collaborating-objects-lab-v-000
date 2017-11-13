d83c8883852da02bb67e6f194c3734f535546062
Morgan Fudge <mfudge92@gmail.com>
2017-11-13 20:10:27 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(name)
+    title = name.chomp(".mp3").split(" - ")
+    song = Song.new(title[1])
+    song.artist = Artist.find_or_create_by_name(title[0])
+    song.artist.add_song(song)
+    song
+  end
+end
