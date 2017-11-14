e8cda53451469d0faf8fd2da27494a5a9fe5ddad
Parker Catalano <catalano.work@gmail.com>
2017-11-14 18:57:44 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,34 @@
+require 'pry'
+
 class Artist
   attr_accessor :name, :songs
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @songs = []
   end
 
-  def songs(song)
-    song = Song.new(song)
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+    self
+  end
+
+  def self.find_or_create_by_name(name)
+      binding.pry
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
   end
 
 end
