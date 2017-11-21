be82c7fbef032fcdacf82f1c9c8f025bf27e0740
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-21 22:44:07 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist
+
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+  if self.all.find { |artist| artist.name == name}
+      self.all.find {|artist| artist.name == name}
+    else
+    artist = self.new(name)
+    artist.save
+    artist
+    end
+  end
+
+  def print_songs
+    self.songs.each {|song| puts song.name}
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.entries(path).select do |file|
+       file.include?('mp3')
+     end
+  end
+
+  def import
+    files.each do |file|
+      Song.new_by_filename(file)
+    end
+  end
+
+
+end
