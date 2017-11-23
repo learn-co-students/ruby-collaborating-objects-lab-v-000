0be0acd13a3f71e1600cdb3d7470f95356ff62b0
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-23 16:01:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,20 @@ class Song
 
   def initialize(name)
     @name = name
+    @artist = artist
+  end
 
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
   end
 
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
+    song.artist_name=(filename.split(" - ")[0])
+    song
   end
 end
