ebcf805ae29b7b79b818eb55aa2bbb90e8834190
Laura Bodell <bodelllaura@gmail.com>
2017-12-01 22:47:39 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+   attr_accessor :name, :artist
+
+   def initialize(name)
+     @name = name
+     @artist = artist
+   end
+
+   def artist_name=(name)
+     if (self.artist.nil?)
+       self.artist = Artist.find_or_create_by_name(name)
+       self.artist.add_song(self)
+     else
+       self.artist.name = name
+     end
+   end
+
+   def self.new_by_filename(filename)
+     song = self.new(filename.split(" - ")[1])
+     song.artist_name=(filename.split(" - ")[0])
+     song
+  end
+ end
