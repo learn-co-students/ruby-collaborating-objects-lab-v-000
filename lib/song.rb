cc13e911b4a55b5f43c6e8dce2744474d2384d60
Liz Burton <liz.burton147@gmail.com>
2017-12-02 20:15:38 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,20 +7,19 @@ attr_accessor :name, :artist_name, :artist
     @name = name
   end
 
-  # def artist=(artist)
-  #   @artist = artist
-  # end
+  def artist=(artist_name)
+    @artist = Artist.find_or_create_by_name(artist_name)
+  end
 
   def self.new_by_filename(filename)
     data = filename.split("-")
     artist_name = data[0].strip
     song_name = data[1].strip
-    song = Song.new(song_name)
-    current_artist = Artist.find_or_create_by_name(artist_name)
-    song.artist = current_artist
-    binding.pry
-    current_artist.add_song(song)
-    current_artist.save
+    @song = Song.new(song_name)
+    @song.artist = artist_name
+    # binding.pry
+    # current_artist.add_song(song)
+    # current_artist.save
     # artist.genre = data[2].sub(".mp3","").strip
   end
 
