0465d025b516fb4122cf9f133208441feeff7b2a
Remmy Clay <remmy.lauren@gmail.com>
2017-12-05 05:09:41 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class MP3Importer
 
-  attr_accessor :path, :file_names
+  attr_accessor :path, :files
 
   def initialize(path)
     @path = path
@@ -13,7 +13,7 @@ class MP3Importer
     @file_names = files.collect {|file| file.split("mp3s/")[1]}
   end
 
-  def import
+  def import(files)
     @file_names.each do |file_name|
     Song.new_by_filename(file_name)
     end
