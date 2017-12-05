5871898e5b242279fba881329caedaeaa8a7c400
Tyler Pulse <tyler3321@gmail.com>
2017-12-05 21:49:15 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,11 @@ class Artist
     end
     
     def self.find_or_create_by_name(name)
-        self.find(name) ? self.find(name) : self.create(name)
+        if self.find(name) 
+            self.find(name)
+        else
+            self.create(name)
+        end 
     end
 
     def self.find(name)
