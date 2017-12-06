01842f1e99af1b6d6ecef478537b76b25e5435af
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-06 20:41:57 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,6 @@
 class MP3Importer
 
-  attr_accessor :path, :files
+  attr_reader :path
 
   def initialize(path)
     @path = path
@@ -13,11 +13,11 @@ class MP3Importer
       File.extname(entry) == ".mp3"
     end
 
-    self.files = mp3_files
+    @files = mp3_files
   end
 
   def import
-    @files.each do |filename|
+    self.files.each do |filename|
       Song.new_by_filename(filename)
     end
   end
--- lib/song.rb
+++ lib/song.rb
@@ -4,21 +4,14 @@ class Song
 
   def initialize(name)
     @name = name
-    @artist = nil
   end
 
-  def artist=(artist_name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(artist_name)
-    else
-      self.artist.name = artist_name
-    end
-  endx
-
-  def self.new_by_filename
-    song = self.new
-    song.title = filename.split(" - ")[1]
-    song
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist = Artist.find_or_create_by_name(artist)
+    new_song.artist.add_song(new_song)
+    new_song
   end
-  
+
 end
