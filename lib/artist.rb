ce3b0406f247aec5f4bdf8c2ee28cd0a96bf9c7e
Antonio Reid <read.musik@gmail.com>
2017-12-10 04:49:54 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,46 @@
+require 'pry'
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    if @@all.detect {|artist| artist.name == artist_name}.nil?
+      artist = Artist.new(artist_name)
+      artist.save
+      artist
+      #binding.pry
+    else
+      @@all.detect {|artist| artist.name == artist_name}
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts "#{song.name}"
+    end
+  end
+
+end
