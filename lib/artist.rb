f96e1c8ab22461222468c0c49767d01f532aa1f6
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-19 22:06:13 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+require "pry"
+class Artist
+@@all = []
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    @@all.find {|a| a.name = name}
+  end
+
+  def self.create(name)
+    new_art = self.new(name)
+    new_art.save
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def print_songs
+    songs.each {|s| puts s.name}
+  end
+
+
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+
+  attr_accessor :path, :size
+
+
+def initialize(path)
+  @path = path
+end
+
+def files
+  
+end
+
+def import
+
+end
+
+end
