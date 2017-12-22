589158eecb8b36ddd84a192686c8fd5144f9b76c
rh24 <ms.rebeccahong@gmail.com>
2017-12-22 13:25:03 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,46 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|a| a.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.create_by_name(name)
+    # binding.pry
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def print_songs
+    songs.each {|s| puts s.name}
+  end
+end
