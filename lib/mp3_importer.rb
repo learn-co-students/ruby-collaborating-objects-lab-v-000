5c54fb13652e145fca150f26f3c0597913b306d0
Jonathan Luk <jluk87@gmail.com>
2017-12-24 23:50:45 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,13 +11,11 @@ class MP3Importer
   end
 
   def files
-    self.filenames = Dir["#{self.path}/*.mp3"]
-    self.filenames.collect! {|filename| File.basename(filename)}
+    self.filenames = Dir["#{self.path}/*.mp3"].collect! {|filename| File.basename(filename)}
   end
 
   def import
-    self.files
-    self.filenames.collect{|filename| Song.new_by_filename(filename)}
+    files.collect{|filename| Song.new_by_filename(filename)}
   end
 
 end
