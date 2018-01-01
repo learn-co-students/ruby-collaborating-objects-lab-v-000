046dbf75de8dc79fe544f8bafe582e859aa7dd24
Robert Watson <spartan83@gmail.com>
2018-01-01 04:05:37 +0000

    Done.

--- spec/mp3_importer_spec.rb
+++ spec/mp3_importer_spec.rb
@@ -37,7 +37,7 @@ describe "MP3Importer" do
       music_importer = MP3Importer.new(test_music_path)
       music_importer.import
       expect(Artist.all.size).to eq(3)
-      # expect(Artist.all.first.songs.empty? && Artist.all.last.songs.empty?).to eq(false)
+      expect(Artist.all.first.songs.empty? && Artist.all.last.songs.empty?).to eq(false)
       end
   end
 end
