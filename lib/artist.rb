cbd9bd77046dde06f4a3676211a2163d737e89e9
Man Leung <lepusleung@gmail.com>
2018-01-11 16:34:31 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,43 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|artist| artist.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+  end
+
+  def print_songs
+    songs.each do |song|
+      puts song.name
+    end
+  end
+
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files = Dir.glob("#{path}/*.mp3").map do |file|
+      file.gsub("#{path}/", "")
+    end
+  end
+
+  def import
+    files.each do |file|
+      Song.new_by_filename(file)
+    end
+  end
+
+end
