768de6442c60fdb18fb764db61af30cb6268322c
Ingrid Wong <yw340@georgetown.edu>
2018-01-19 16:38:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 class Song
   attr_accessor :name, :artist
 
@@ -7,23 +5,18 @@ class Song
     @name = name
   end
 
-
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
     artist = file.split(" - ")[0]
-    #binding.pry
-    song = Song.new(song_name)
+    song = self.new(song_name)
     song.artist_name = artist
     song
   end
 
   def artist_name=(artist_name)
-    #takes in a string of artist name and returns the artist instance
-    artist = Artist.find_or_create_by_name(artist_name)
-    #binding.pry
+    artist = self.find_or_create_by_name(artist_name)
     artist.add_song(self)
-    #binding.pry
     self.artist = artist
-    artist
+    #artist
   end
 end
