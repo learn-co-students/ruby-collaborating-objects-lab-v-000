65cdab2847a3ff6de1ddd50333f401ea7590cc95
avelineamour <razelle@satyaorthopedic.com>
2018-01-22 23:52:17 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,27 +4,13 @@ class Song
 
   def initialize(name)
     @name = name
-    # self.artist = artist
   end
 
-  # def artist=(artist)
-  #   @artist = artist
-  #   artist.add_song(self)
-  # end
 
   def artist_name(name)
    self.artist = Artist.find_or_create_by_name(name)
-   # I moved the artist.find_or_create here from new_by_filename
    self.artist.add_song(self)
  end
-
-  # def self.new_by_filename(filename)
-  #   song = self.new(filename)
-  #   song.name = filename.split(" - ")[1]
-  #   artistname = filename.split(" - ")[0]
-  #   song.artist = Artist.find_or_create_by_name(artistname)
-  #   song
-  # end
 
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
