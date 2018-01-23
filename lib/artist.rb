307264b08cd6c9fef758cd440c240e806d7d3e65
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-23 20:14:13 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,14 +12,33 @@ class Artist
     @songs
   end
 
-  def self.save
+  def self.all
     @@all
   end
 
-  def find_or_create_by_name
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name}
+  end
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
   end
 
   def print_songs
+    songs.each {|song| puts song.name}
   end
 
 end
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,15 @@
 class MP3Importer
-  attr_accessor
+  attr_accessor :path
 
-  def initialize
+  def initialize(path)
+    @path = path
   end
 
+  def files
+    @files ||=Dir.glob("#{path}/*.mp3").collect{|f| f.gsub("#{path}/","")}
+  end
+
+  def import
+    files.each {|f| Song.new_by_filename(f)}
+  end
 end
