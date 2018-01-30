b135755f75290a1e572740e03b3ef217be26807a
Stephen Thurman <thurman024@gmail.com>
2018-01-30 20:40:30 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist
+  # attr_reader :
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+    song_title = filename.split(" - ")[1]
+    song_artist = filename.split(" - ")[0]
+    song = Song.new(song_title)
+    song.artist=(song_artist)
+    # binding.pry
+    song
+  end
+
+  def artist=(name)
+    binding.pry
+    if name.kind_of?(String)
+      artist = Artist.find_or_create_by_name(name)
+
+      artist.add_song(self)
+
+    else
+      name.add_song(self)
+    end
+
+  end
+end
