02ed03e26db2519dea9d599d97b4b5f6c30bc78b
Ron Sala <ron.sala@gmail.com>
2018-02-01 11:28:10 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,37 +16,15 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def songs
     @songs
   end
 
-  def self.song_count
-    @@song_count
-  end
-
   def save
     self.class.all << self
   end
-
-  #
-#   def self.create_by_name(name)
-#   song = self.create
-#   song.name = song_name
-#   song
-# end
-
-def self.find_by_name(name)
-  self.all.detect{|a| a.name == name}
-end
-
-# def self.find_or_create_by_name(song_name)
-#   self.find_by_name(song_name) || self.create_by_name(song_name)
-# end
-
-#
 
   def self.find_or_create_by_name(name)
     @@all.detect{|e| e.name == name} || name = Artist.new(name)
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    
+  end
+
+  def import
+    Song.new_by_filename(some_filename)
+  end
+end
