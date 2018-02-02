33c0e7f41dd1a1f7e57d0a7ee1530ac38c61bb15
Ron Sala <ron.sala@gmail.com>
2018-02-02 15:28:37 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,18 +2,18 @@ require "pry"
 
 class MP3Importer
 
-  attr_accessor :path
+  attr_accessor :path, :files
 
   def initialize(path)
     @path = path
   end
 
   def files
-    Dir.entries(path).select{|f| f.include?(".mp3")}
+    @files = Dir.entries(path).select{|f| f.include?(".mp3")}
   end
 
   def import
     # binding.pry
-    files.each{ |filename| Song.new_by_filename(filename) }
+    @files.each{ |filename| Song.new_by_filename(filename) }
   end
 end
