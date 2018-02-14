bc11451b7cce329bc53ce6e6997f49b32cfc0b11
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-14 21:03:39 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+  attr_accessor :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    Dir.entries(@path).select {|filename| filename.include?(".mp3")}
+  end
+
+  def import
+    self.files.each do |filename|
+      song = Song.new_by_filename(filename)
+      Artist.all << song.artist unless Artist.all.include?(song.artist)
+    end
+  end
+end
