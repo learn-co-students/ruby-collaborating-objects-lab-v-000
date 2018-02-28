511997d0c8aad03792872ee4338da3e0edce1e74
Andrew Hong <hongandrew16@gmail.com>
2018-02-28 22:38:59 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,23 @@
+require 'pry'
 class MP3Importer
-  attr_reader :path
+  attr_accessor :path
 
+  # path = './db/mp3s'
   def initialize(path)
     @path = path
   end
 
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    @files = Dir.entries(@path)
+    @files.delete_if do |file|
+      file == "." || file == ".."
+    end
   end
 
   def import
-    files.each{|f| Song.new_by_filename(f)}
+    files.each do |file|
+      Song.new_by_filename(file)
+    end
   end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -6,14 +6,19 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1]
+
+    song = self.new(song_name)
+    song.artist_name = artist_name
+    song
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
+
+
 end
