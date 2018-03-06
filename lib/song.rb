5dce164374c3c9fb2074623aee6fa55fb82e689d
Marin Cabac <marin.cabac@gmail.com>
2018-03-06 17:52:39 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+  #  arr = filename.split(" - ")
+  # artist = x[0]
+#  song_title = x[1]
+ song = self.new(filename.split(" - ")[1])
+ song.artist_name = (filename.split(" - ")[0])
+  song
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
+end
