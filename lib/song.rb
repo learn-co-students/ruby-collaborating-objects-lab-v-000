7604c9997d478dbd86a3464f539b5be0f07817e7
Robert W <robwash@gmail.com>
2018-03-07 23:09:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,17 +5,16 @@ class Song
 
   def initialize(name)
     @name = name
-    #@artist = nil
   end
 
   def self.new_by_filename(filename)
     song_arr = filename.split(" - ")
-
     song = self.new("#{song_arr[1]}")
-    #art_obj = Artist.find_or_create_by_name("#{song_arr[0]}")
     song.artist_name = "#{song_arr[0]}"
-    song.artist = art_obj
-    art_obj.songs << song
+
+    #art_obj = Artist.find_or_create_by_name("#{song_arr[0]}")
+    #song.artist = art_obj
+    #art_obj.songs << song
     song
   end
 
@@ -23,7 +22,6 @@ class Song
     art_obj = Artist.find_or_create_by_name(name)
     self.artist = art_obj
     artist.add_song(self)
-
   end
 
 end # => End Song Class
