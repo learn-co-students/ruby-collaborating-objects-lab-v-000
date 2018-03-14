75a6bdcfdc883a3393cf10617dffe6fb7a895226
Brandon Brasson <rocksong622@gmail.com>
2018-03-14 23:20:05 +0000

    Done.

--- spec/mp3_importer_spec.rb
+++ spec/mp3_importer_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'pry'
 
-describe "MP3Importer" do
+puts describe "MP3Importer" do
   describe '#initialize' do
     it 'accepts a file path to parse mp3 files from' do
       test_music_path = "./spec/fixtures/mp3s"
@@ -30,7 +30,7 @@ describe "MP3Importer" do
     end
   end
 
-  describe '#import' do 
+  describe '#import' do
     it 'imports the files into the library by creating songs from a filename' do
       Artist.class_variable_set("@@all",[])
       test_music_path = "./spec/fixtures/mp3s"
