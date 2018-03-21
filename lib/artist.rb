1378f804164f2c1032f162307fb45482cb1038ff
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-03-21 02:57:13 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,7 @@ class Artist
 
   def save
     @@all << self
-
+ 
   end
 
   def self.all
--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,11 @@ class Song
 
   end
 
-  def self.new_by_filename(name)
-
+  def self.new_by_filename(filename)
+    split_name = filename.split(" - ")
+    song = self.new(split_name[1])
+    artist = Artist.find_or_create_by_name(split_name[0])
+    # song.artist = artist 
   end
 
 
