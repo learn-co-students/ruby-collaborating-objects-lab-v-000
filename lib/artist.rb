e7ed1648f4f08061937f34f4e48964ca0b8c11fa
Brandon Chin <brandonchin@me.com>
2018-03-29 04:13:52 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :song
   @@all =[]
 
   def initialize(name)
--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,17 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist
 
-  def initialize(name, artist = nil)
+  def initialize(name)
     @name = name
-    @artist = artist
-    @genre = genre
   end #def initialize
 
   def self.new_by_filename(filename)
     filename_array = filename.split(" - ")
-    song = self.new(filename_array[1], @artist)
-    @artist = Artist.find_or_create_by_name(filename_array[0])
-    genre = filename_array[2]
-    @artist.add_song(song)
-    @artist.save
-    song.artist = @artist
+    song = self.new(filename_array[1])
+    artist = Artist.find_or_create_by_name(filename_array[0])
+    song.artist = artist
+    artist.add_song(song)
+    artist.save
     song
   end #def new_by_filename
 
