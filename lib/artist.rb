f144796ccebf8e1511e81db2acb417b3035559d9
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-29 20:34:25 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,16 +24,13 @@ def self.all
 end
 
 def self.find_or_create_by_name(name)
-self.find(name) ? self.find(name) : self.create(name)
+self.find(name) ? self.find(name) : self.new(name)
 end
 
 def self.find(name)
   self.all.find {|artist| artist.name == name}
 end
 
-def self.create(name)
-  self.new(name).tap {|artist| artist.save}
-end
 
 def print_songs
 songs.each {|song| puts song.name}
