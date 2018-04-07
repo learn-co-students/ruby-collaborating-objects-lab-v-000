d46af1ea1789388da3e638973cf5ee4edca1ade3
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-07 21:01:22 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,12 +7,24 @@ class MP3Importer
   end
 
   def files
-    
+      Dir.glob("#{path}/*").map do |name|
+        name.split("/")[4]
+    end
   end
 
   def import
+    files.each do |file|
+      parts = file.split(" - ")
+      artist_name = parts [0]
+      song_name = parts[1]
+      genre_name = parts[2]
+      s = Song.new(song_name)
+      
 
+      a = Artist.find_or_create_by_name(artist_name)
+      a.add_song(Song.new(song_name))
   end
+end
 
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -7,4 +7,10 @@ class Song
     @name = name
   end
 
+  def self.new_by_filename(file_name)
+    new_song = Song.new(file_name.split(" - ")[1])
+    new_artist = Artist.new(file_name.split(" - ")[0])
+   return new_song
+  end
+
 end
