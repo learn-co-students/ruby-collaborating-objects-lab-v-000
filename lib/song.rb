5f8008be772879701f8f4e317853675af71b3727
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-09 13:36:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -14,12 +14,12 @@ attr_reader :name
     @name = name
   end
 
-  def self.new_by_filename(filename)
-      artist, song = filename.split(" - ")
-      new_song = self.new(song)
-      new_song.artist_name = artist
-      new_song
-    end
+#  def self.new_by_filename(filename)
+#      artist, song = filename.split(" - ")
+#      new_song = self.new(song)
+#      new_song.artist_name = artist
+#      new_song
+#    end
 
 def artist_name=(name)
   self.artist = Artist.find_or_create_by_name(name)
@@ -27,10 +27,10 @@ def artist_name=(name)
 end
 
 
-#def self.new_by_filename(file)
-#  song = self.new(file.split(" - ")[1])
-#  song.artist_name = file.split(" - ")[0]
-#  song
-#end
+def self.new_by_filename(file)
+  song = self.new(file.split(" - ")[1])
+  song.artist_name = file.split(" - ")[0]
+  song
+end
 
 end
