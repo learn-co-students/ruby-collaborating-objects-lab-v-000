fea1de8708880ff8291eed12206f5bda86d30f5f
woollymammoth1 A <isaacatwood@outlook.com>
2018-04-09 22:30:11 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,17 @@
 class Artist
   attr_accessor :name, :songs
+  @@all = []
 
   def initalize(name)
     @name = name
     @songs = []
   end
+
+  def add_song(name)
+    @songs << name
+    name.artist = self
+  end
+
+
 
 end
