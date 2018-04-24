10c040fdaabada3da7d82f9ec8355a0b31118904
Diogo Ide <diogoide@gmail.com>
2018-04-24 23:30:51 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,11 @@
+class MP3Importer
+
+  attr_accessor :filepath
+
+  def initialize(filepath)
+    mp3importer = MP3Importer.new(filepath)
+  end
+
+
+
+end
