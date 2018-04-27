6822ee2f878845ba56fdb3b4bce3724baef4f63f
Trisha Garrett <trisha.garrett@gmail.com>
2018-04-27 21:17:01 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,7 @@ class Song
     @name = name
   end
 
-  def self.new_by_filename(name)
-    song = self.new(name)
-    song.title = name.split(" - ")[1]
-    song
+  def self.new_by_filename(filename)
+    song = self.new(filename)
   end
 end
