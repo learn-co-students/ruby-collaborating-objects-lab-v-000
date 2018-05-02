da50b049f0b95e366a30b919931a96c7a4f2a2ea
Kitty Davies <kittyfdavies@gmail.com>
2018-05-02 23:45:26 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,17 @@
 require 'pry'
 class Artist
+
   attr_accessor :name, :title
   @songs = []
-
+  @@all = []
 
   def initialize(name)
     @name = name
   end
-
+  #take the Song instance that is passed in as an argument
+  #and store it in a @songs array w/ all songs by the artist
   def add_song(song)
-
+    @songs << song
   end
 
   def songs
@@ -17,9 +19,9 @@ class Artist
   end
 
   def save
-
+    @@all << self
   end
-
+  #find the artist instance that has that name or create one if it doesn't exist
   def self.find_or_create_by_name(name)
     if (self.name.nil?)
       self.name = Artist.new(name)
@@ -29,7 +31,7 @@ class Artist
   end
 
   def print_songs
-
+    puts @songs
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,21 @@
 require 'pry'
 class Song
-
+  #name= sets the song name
   attr_accessor :name, :artist
-
+  #initialize with #name
   def initialize(name)
     @name = name
   end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-  end
-
+  #.new_by_filename creates a new song instance from the passed-in file
   def self.new_by_filename(filename)
     song = self.new(name)
     song.name = filename.split(" - ")[1]
-    #song.artist_name = filename.split(" - ")[0]
-    song.name
+    song
     #binding.pry
+  end
+  #turn the artist's name string into an artist object/assign the song to the artist
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
   end
 
 end
