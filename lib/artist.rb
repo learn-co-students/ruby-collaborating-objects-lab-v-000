c397f9daa01546c9db92e58182e2bef919f2b36d
Kitty Davies <kittyfdavies@gmail.com>
2018-05-08 00:27:31 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,38 +8,45 @@ class Artist
     @name = name
     @songs = []
   end
-  #take the Song instance that is passed in as an argument
-  #and store it in a @songs array w/ all songs by the artist
+
   def add_song(song)
     @songs << song
-    #song.artist = self
   end
 
   def songs
     @songs
   end
 
-  #adds the artist instance to the @@all class variable
   def save
     @@all << self
   end
 
-  def all
+  def self.all
     @@all
   end
 
-  #find the artist instance that has that name or create one if it doesn't exist
-  def self.find_or_create_by_name(name)
-    #binding.pry
-    if (self.name.nil?)
-      self.name = Artist.new(name)
-    else
-      self.name
+  def self.find_by_name(name)
+    @@all.detect do |match|
+    match.name == name
     end
   end
 
+  def self.create_by_name(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
   def print_songs
-    puts @songs
+    ary = []
+    @songs.each {|song|
+      ary << song.instance_variable_get(:@name)}
+    puts ary
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,30 @@
 require 'pry'
 class Song
-  #name= sets the song name
+
   attr_accessor :name, :artist
-  #initialize with #name
+
   def initialize(name)
     @name = name
   end
+
+  def self.song_from_filename(filename)
+    name = filename.split(" - ")[1]
+    name
+  end
+
+  def self.artist_from_filename(filename)
+    artist = filename.split(" - ")[0]
+    artist
+  end
+
   #.new_by_filename creates a new song instance from the passed-in file
   def self.new_by_filename(filename)
+    binding.pry
+    song_name = filename.split(" - ")[1]
+    song_name
     song = self.new(name)
-    song.name = filename.split(" - ")[1]
-    song.artist = filename.split(" - ")[0]
-    song
   end
-  #turn the artist's name string into an artist object/assign the song to the artist
+
   def artist_name=(name)
     self.artist.name = Artist.find_or_create_by_name(name)
   end
