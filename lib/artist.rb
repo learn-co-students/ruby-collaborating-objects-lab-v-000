73b4d504394d34e0409ff1ef3b4d199c305e26d2
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-09 22:13:39 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,12 +21,20 @@ class Artist
     self.class.all << self
   end
   #binding.pry
-  def self.find_or_create_by_name
-
+  def self.find(name)
+    self.all.detect {|a| a.name == name}
   end
 
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end
 
   def print_songs
 
   end
+  binding.pry
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,12 @@
 class Song
-  attr_accessor :name, :artist, :files
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
 
   def self.new_by_filename(filename)
-    file_name = filename.split(" - ")
-    name = file_name[1].gsub(".mp3", "")
-    song = self.new(name)
-    song.artist_name = file_name[0]
-    song
+    newfile = filename.split(" - ")
+    binding.pry
   end
 end
