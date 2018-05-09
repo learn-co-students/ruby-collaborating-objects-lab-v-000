3a2ff532fe4846992fdea982be395400b306b2b8
Kitty Davies <kittyfdavies@gmail.com>
2018-05-09 20:54:42 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -12,12 +12,13 @@ class Song
     artist = filename.split(" - ")[0]
     song_name = filename.split(" - ")[1]
     song = Song.new(song_name)
-    song.artist = artist_name=(artist)
+    song.artist_name=(artist)
     song
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
+    #binding.pry
     artist.add_song(self)
   end
 
