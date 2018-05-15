926489358ca107bca8ce664948f49897cf7c2c08
Scott Weinstein <sweinstein27@gmail.com>
2018-05-15 19:51:55 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+require "pry"
+
+class Artist
+  attr_accessor :name, :songs
+
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+end
+
+  def self.all
+    @@all
+  end
+
+def self.find_or_create_by_name(name)
+  self.all.each do |artist|
+    if artist.name == name
+      return artist
+    else
+      Artist.new
+    end
+  end
+end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+
+attr_accessor :name, :artist
+
+def initialize(name)
+  @name = name
+end
+
+
+end
