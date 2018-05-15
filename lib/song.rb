a8501e6c3ce819b971f763dddf37407005af926a
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 17:47:47 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+    artist_name, song_name, extra = filename.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
+  end
+end
