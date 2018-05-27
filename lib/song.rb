a53815153835b6feeab4571f7c185f4e4d014ac6
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-27 15:16:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,18 +9,12 @@ class Song
     @name = name
   end
 
-  def artist=(artist)
-    artist = Artist.find_or_create_by_name(artist)
-    @artist = artist
-  end
-
   def self.new_by_filename(file_name)
     file_name = file_name.chomp(".mp3").split(" - ")
     name = file_name[1]
     song = Song.new(name)
-    song.artist = file_name[0]
-    binding.pry
-    song.artist.add_song(song)
+    song.artist = Artist.find_or_create_by_name(file_name[0])
+    song.artist.add_song(song.name)
     song.genre = file_name[2]
     song
   end
