d7e7fedbca81d75696af739c0e7dde1bef46fe8d
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-31 02:36:43 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,6 @@
+class MP3Importer
+
+  def initialize
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name=(artist_name)
+    @artist.name = self.artist.find_or_create_by_name(artist_name)
+    @artist.add_song(self)
+  end
+
+  def self.new_by_filename(filename)
+    artist_name_s = filename.split(" - ")[0]
+    name_s = filename.split(" - ")[1]
+    song = self.new(name_s)
+    song.name = name_s
+    song.artist_name = artist_name_s
+    song
+  end
+
+end
