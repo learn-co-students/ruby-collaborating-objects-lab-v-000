8a853f1da88442721c246d35721d2088420b10d5
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-01 07:48:56 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,21 +1,18 @@
+require 'pry'
 class MP3Importer
 attr_accessor :path
 
   def initialize(path)
     @path = path
-    @mp3s = []
   end
 
   def files
-    @mp3s_without_path = []
-    @mp3s = Dir["./spec/fixtures/mp3s/*.mp3"].each {|file| @mp3s << file}
-    @mp3s.each do |mp3|
-      @mp3s_without_path << mp3.split("mp3s/")[1]
-    end
-    @mp3s_without_path
+    filenames = []
+    Dir[File.join(self.path, "*.mp3")].each {|file| filenames << file.split('mp3s/')[1]}
+    filenames
   end
 
   def import
-    Song.new_by_filename(@mp3s_without_path)
+    self.files.each {|filename| Song.new_by_filename(filename)}
   end
 end
