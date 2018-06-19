a277b091fab8ee0736194ae9a5b4a936a0d97c8a
Brandon Green <brandon@brandonleeproductions.com>
2018-06-19 23:00:37 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+# Will come back to this later...
+
+require 'pry'
+
+class Artist
+
+  @@all = []
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end
+
+  def add_song(song_name)
+    @songs << song_name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    @@all.each {|artist| return artist if artist.name == artist_name}
+    Artist.new(artist_name)
+  end
+
+  def print_songs
+
+    self.songs.each do |song|
+      puts song
+    end
+    # binding.pry
+    # songs.each {|song| puts song}
+
+    # binding.pry
+  end
+
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,19 @@
+require "pry"
+
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(file)
+    @path = file
+  end
+
+  def files
+    Dir.entries(path).select {|file| file.end_with?(".mp3")}
+  end
+
+  def import
+    files.each {|file| Song.new_by_filename(file)}
+    # binding.pry
+  end
+end  
