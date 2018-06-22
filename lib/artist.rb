0c82b3df3daf801b1fcead75ef2d1df0da777484
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-06-22 04:08:10 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,51 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def self.find(name)
+    @@all.detect{ |artist| artist.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+
+
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+    song_data = filename.chomp(".mp3").split(" - ")
+    song = self.new(song_data[1])
+    song.artist_name = song_data[0]
+    song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+
+
+
+
+end
