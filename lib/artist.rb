08a18cce48fddd7d5292283b3e80ab214abad5ef
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-26 18:18:21 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,47 @@
+class Artist
+  
+  attr_accessor :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    self.songs << song
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def find_or_create_by_name(name)
+    if self.find name = name
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+  
+  def self.find(name)
+    self.all.detect {| artist | artist.name == name}
+  end
+  
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+  
+  def print_songs
+    self.songs.each {| song | song.name}
+  end
+  
+  
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+class Song
+  
+end
+
+
+
+class MP3Importer
+  
+  def import(list_of_filenames)
+#    list_of_filenames.each do {| filename | #song.new_by_filename(filename)}
+  end
+  
+end
+
+
