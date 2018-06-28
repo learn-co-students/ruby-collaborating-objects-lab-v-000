a52545a612bac72df63be8d988d751bd2d1a2a3c
Reno Sorensen <renojames19@gmail.com>
2018-06-28 01:52:34 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,42 @@
+
+
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def print_songs
+    puts @songs.name.to_s
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    if
+      self.all.find{ |artist| artist.name == artist_name}
+    else
+      artist = Artist.new(artist_name)
+    end
+  end
+
+end
