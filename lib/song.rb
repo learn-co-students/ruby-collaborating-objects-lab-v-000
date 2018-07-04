0464763f32b588e36ed4364df87327a3169601fa
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-04 13:51:50 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) if !artist.songs.include?(self)
+  end
+
+  def self.new_by_filename(filename)
+    artist = filename.split(" - ")[0]
+    song_name = filename.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name=(artist)
+    song
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+end
