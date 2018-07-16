0189ec1959e2107aad52c1ccd6ebe46b895d8130
Wendy Calderon <wendycalderon@gmail.com>
2018-07-16 04:10:04 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,7 +13,6 @@ class MP3Importer
       if file_name.end_with?(".mp3")
         file_name=file_name.gsub("./spec/fixtures/mp3s/", "")
         all_files << file_name
-        
       end
     end
     all_files
@@ -21,6 +20,13 @@ class MP3Importer
   end
   
   def import 
-    
+    self.files.each do |file|
+      file=file.split(" - ")
+      artist_name=file[0]
+      song_name=file[1]
+      genre_name=file[2]
+      Song.new(song_name)
+      #find or create an artist for the song
+    end
   end
 end
