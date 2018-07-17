899da93c32e1d5215119d60eaedecde284faf8aa
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-07-17 15:58:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,8 @@
 class Song
   attr_accessor :name, :artist
-  @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self
-  end
-
-  def artist_name
-    if artist.nil?
-      nil
-    else
-      artist.name
-    end
   end
 
   def self.new_by_filename(filename)
