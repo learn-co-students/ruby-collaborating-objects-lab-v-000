c7c4261d6ad0609e4b2e10450a6b6f9989028afc
Stavroula Guerrero <4stacyg@optonline.net>
2018-07-20 07:45:06 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(file)
+    song = Song.new(file.split(" - ")[1])
+    song.artist = Artist.find_or_create_by_name(file.split(" - ")[0])
+    song.artist.add_song(self)
+    song
+  end
+end
