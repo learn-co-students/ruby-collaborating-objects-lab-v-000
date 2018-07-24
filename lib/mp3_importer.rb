f20ace965e49adf86e8a19235550c0d97c55fc9a
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-24 22:55:44 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path 
+  end 
+  
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect {|f| f.gsub("#{path}/", "")}
+  end 
+  
+  def import 
+    files.each {|f| Song.new_by_filename(f)}
+  end 
+  
+end 
