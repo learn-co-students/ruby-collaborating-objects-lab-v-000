797228494d89c6b55c1746fb607a0d0459ca82e1
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-10 04:08:48 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,5 +5,32 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.detect {|artist| artist.name == name} != nil
+      return @@all.detect {|artist| artist.name == name}
+    else
+      return self.new(name)
+    end
+  end
+
+  def add_song(song)
+    @songs << song
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,12 @@ class Song
     split_filename = filename.split(" - ")
     song = Song.new(split_filename[1])
     song.artist_name = split_filename[0]
+    return song
+  end
+
+  def artist_name=(name)
+    @artist = Artist.find_or_create_by_name(name)
+    @artist.add_song(self)
   end
 
 end
