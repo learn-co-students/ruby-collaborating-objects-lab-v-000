d47cb0a453137454fe6da067c326835e363c5577
Angie Kassab <anasab12@gmail.com>
2018-08-18 22:48:15 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,48 +1,3 @@
-# class Artist
-#   attr_accessor :name
-#   attr_reader :songs
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#   end
-
-#   @@all = []
-
-#   def self.all
-#     @@all
-#   end
-
-#   def self.find_or_create_by_name(name)
-#     self.find_by_name(name) || self.create_by_name(name)
-#   end
-
-#   def self.find_by_name(name)
-#     @@all.detect do |artist|
-#       artist.name == name
-#     end
-#   end
-
-#   def self.create_by_name(name)
-#     self.new(name).save
-#   end
-
-#   def add_song(song)
-#     @songs.push(song) unless @songs.include?(song)
-#     song.artist = self unless song.artist == self
-#   end
-
-#   def save
-#     @@all.push(self)
-#     self
-#   end
-
-#   def print_songs
-#     @songs.each {|s| puts s.name }
-#   end
-
-# end
-
-
 class Artist
   attr_accessor :name
   attr_reader :songs
@@ -91,3 +46,49 @@ class Artist
     end
   end
 end
+
+# alternate solution
+
+# class Artist
+#   attr_accessor :name
+#   attr_reader :songs
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
+
+#   @@all = []
+
+#   def self.all
+#     @@all
+#   end
+
+#   def self.find_or_create_by_name(name)
+#     self.find_by_name(name) || self.create_by_name(name)
+#   end
+
+#   def self.find_by_name(name)
+#     @@all.detect do |artist|
+#       artist.name == name
+#     end
+#   end
+
+#   def self.create_by_name(name)
+#     self.new(name).save
+#   end
+
+#   def add_song(song)
+#     @songs.push(song) unless @songs.include?(song)
+#     song.artist = self unless song.artist == self
+#   end
+
+#   def save
+#     @@all.push(self)
+#     self
+#   end
+
+#   def print_songs
+#     @songs.each {|s| puts s.name }
+#   end
+
+# end
--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
 
   def artist=(artist)
     @artist = artist
-    artist.add_song(self) unless artist.songs.include?(self) #if the song is already there, don't include it so that you don't have 5 instances of "Halo", for example.
+    artist.add_song(self) unless artist.songs.include?(self)
   end
 
   def self.new_by_filename(filename)
@@ -32,108 +32,3 @@ class Song
   end
 
 end
-
-# require 'pry'
-
-# class Song
-#   attr_accessor :name, :artist
-
-#   def initialize(name) 
-#     @name = name 
-#     @artist = nil #the song doesn't have a relationship with the artist yet.
-#     end
-
-#   def artist_name=(artist_name)
-#     a = Artist.find_or_create_by_name(artist_name)
-#     a.add_song(self)
-#     self.artist = a
-#   end
-  
-  
-#   def self.new_by_filename(file_name)
-#     song = file_name.split(" - ")[1]
-#     artist = file_name.split(" - ")[0]
-#     new_song = self.new(song)
-#     new_song.artist_name = song 
-#     new_song
-#   end
-  
-#   def artist_name=(name)
-#     self.artist = Artist.find_or_create_by_name(name)
-#     artist.add_song(self)
-#   end
-
-
-# end
-
-
-
-# =begin 
-# class Song
-
-#   attr_accessor :name, :artist
-
-#   def initialize(name)
-#     @name = name
-#   end
-
-#   def self.new_by_filename(file_name)
-#     song = file_name.split(" - ")[1]
-#     artist = file_name.split(" - ")[0]
-#     new_song = self.new(song)
-#     new_song.artist_name = song 
-#     new_song
-#   end
-
-#   def artist_name=(name)
-#     self.artist = Artist.find_or_create_by_name(name)
-#     artist.add_song(self)
-#   end
-
-# end 
-# =end 
-
-# =begin
-# class Song
-#   attr_accessor :name, :artist
-#     @@all = []
-
-  
-#   def initialize(name)
-#     @name=name
-#   end
-  
-#   def artist_name=(name)
-#     self.artist = Artist.find_or_create_by_name(name)
-#     artist.add_song(self)
-#   end
-  
-# =begin
-
-#   def artist_name=(artist_name)
-#     if self.artist == nil
-#       self.artist = Artist.find_or_create_by_name(artist_name)
-      
-#     else 
-#       self.artist.name = artist_name
-#     end 
-#   end
-# =end
-# =begin
-  
-#   def self.new_by_filename(filename)
-#     long_song_name = []
-#     long_song_name = filename.split(" - ")
-#     artist_name_extracted = long_song_name[0]
-#     song_name = long_song_name[1]
-    
-#     new_song_instance = self.new(artist_name_extracted)
-#     new_song_instace.name = song_name
-#     new_song_instance.artist = artist_name_extracted
-#     new_song_instance
-    
-
-  
-# end
-# end
-# =end
