6facc0f57d9bf6bef46ff208e3b28cd7c3dc525c
Sharon Mun <sharonymun@gmail.com>
2018-08-20 20:54:17 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,8 @@
+class Artist
+  attr_accessor :name, :songs
+  def initialize(name)
+    @name = name
+    @songs = songs
+  end
+  
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,8 @@
+class MP3Importer
+  attr_accessor :path
+  def initialize
+    @path = path
+  end
+  def self.import
+  end
+end
