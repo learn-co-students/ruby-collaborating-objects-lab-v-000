a6d11f8cbbebd5059ac747aebccbbcba3e705c10
Sharon Mun <sharonymun@gmail.com>
2018-08-21 02:56:35 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,36 @@
 class Artist
   attr_accessor :name, :songs
+  @@all = []
+  def self.all
+    @@all
+  end
   def initialize(name)
     @name = name
-    @songs = songs
+    @songs = []
   end
-  
+  def add_song(song)
+  @songs << song
+  end
+  def save
+    @@all << self
+  end
+  def self.find(find_it)
+    song = @@all.find{|song| song == find_it}
+    song
+  end
+  def self.create(create_it)
+    artist = Artist.new
+    artist.name = create_it
+    artist.save
+    artist
+  end
+  def self.find_or_create_by_name(string)
+    found = self.find(string)
+   created = self.create(string)
+    if found
+      return found
+     else 
+       return created
+     end
+  end
 end
