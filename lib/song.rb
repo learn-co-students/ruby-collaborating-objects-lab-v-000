521be94b145de1f068a721ceea3619905ba6858f
Sharon Mun <sharonymun@gmail.com>
2018-08-22 01:41:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -14,24 +14,24 @@ class Song
     self
   end
   
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(artist)
+  def artist=(artist)
+    @artist = artist
     artist.add_song(self)
   end
   
   def self.new_by_filename(filename)
-    elements = filename.split(" - ")
-    artist = elements[0]
-    song = elements[1]
-    newsong = Song.new(song)
-    newsong.artist_name = artist
-
-    newsong.save
-    # newsong.artist = Artist.create(artist)
-    # newsong.artist.add_song(song)
-    newsong 
+    # elements = filename.split(" - ")
+    # artist = elements[0]
+    # song = elements[1]
+    # newsong = Song.new(song)
+    # newsong.artist_name = artist
+    # newsong.save
+    # # newsong.artist = Artist.create(artist)
+    # # newsong.artist.add_song(song)
+    # newsong 
+    artist_name, song_name = filename.chomp(".mp3").split(" - ")
+    song = self.new(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.save
   end
 end
-
-
-
