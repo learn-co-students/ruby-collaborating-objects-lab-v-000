4c2279ab026f6bfb60741a527fb19823621643ae
Rafael Lopez <lpz.raf@gmail.com>
2018-08-25 19:23:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,15 @@ class Song
   
   def initialize(name)
   @name = name
-  
   end
+  
+  def self.new_by_filename(file)
+      song = self.new
+      # song_info = file.chomp(".mp3").split(" - ")
+      # song.name = song_info[0]
+      song
+    end
+  
+  
 
 end
