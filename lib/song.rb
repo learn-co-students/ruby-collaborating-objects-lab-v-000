47ebc7bd4536afeb1c0b91cbf31068bc9ad3e381
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-01 03:58:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,5 +4,13 @@ class Song
     @name = name
   end
   
+  def new_by_filename(s_name_ext)
+    song = self.new
+    song_parts = s_name_ext.split("- ")
+    song.name = song_parts[1].chomp(".mp3")
+    song.artist_name = song_parts[0].chomp(" ")
+    song
+  end
+  
   
 end
