700335be1de1202e3349042e5cef02574bcaadaf
Adam Taitano <adam.taitano@gmail.com>
2018-09-14 17:31:31 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,16 +19,16 @@ class Artist
     @songs
   end
 
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
-  end
-
   def self.find(name)
     self.all.find {|artist| artist.name == name}
   end
-
+  
   def self.create(name)
     self.new(name).tap {|artist| artist.save}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
   end
 
   def save
