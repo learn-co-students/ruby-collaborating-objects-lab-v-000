4e5314685f98b5a09e0e3173b5e3127d4e63b66c
Nick Romans <nicholas.romans@gmail.com>
2018-09-18 00:07:31 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,21 +9,17 @@ class Song
     @name = name
 end
 
-def save
-  @@all << self
-  self
-end
-
-def artist_name=(name)
-  self.artist = Artist.find_or_create_by_name(name)
-end
-
 def self.new_by_filename(file_name)
   artist, song = file_name.split(" - ")
   new_song = self.new(song)
   new_song.artist_name = artist
-  new_song.save
+  #new_song.save
   new_song
+end
+
+def artist_name=(name)
+  self.artist = Artist.find_or_create_by_name(name)
+  artist.add_song(self)
 end
 
 end 
