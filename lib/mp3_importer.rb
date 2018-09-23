e5f29c41ae6c173be4628caca75ee9dcda2ce4e5
Ashley Wong <ashwong04@gmail.com>
2018-09-23 02:45:12 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,14 +1,18 @@
+require 'pry'
+
 class MP3Importer
   
   attr_accessor :path, :fileNames
   
   def initialize(path)
     @path = path
+    @fileNames = []
   end
   
   def files
-    @fileNames = Dir.entries("mp3s")
+   Dir.new(@path).reject {|s| s == '.'}
   end
+    #binding.pry
   
   def import
   
