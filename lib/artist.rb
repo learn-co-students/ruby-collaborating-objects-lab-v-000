07b99373500403685f2bb467e0ba4a1ec648d224
Stef Claus <stef.claus@gmail.com>
2018-10-09 01:25:58 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,8 +31,10 @@ class Artist
     song.artist = self
   end 
   
-  def print_songs #instance method iterates through @songs of an instance of Artist
-    self.songs.each {|song| song.name}
+  def print_songs 
+    self.songs.each do |song| 
+      puts song.name
+    end 
   end 
 
   
