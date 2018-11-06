38ad2502d3e83916771ecb3de4ba1585a07619c0
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-06 22:39:57 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,25 @@
+require 'pry' 
+
 class Artist
-  attr_accessor
+  attr_accessor :name, :songs
+  @@all=[]
   
+  def initialize(name)
+    @name = name
+    
+  end
+  # binding.pry
+  def add_song(song)
+    song=Song.new
+    @songs << song
+    song.artist=self
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.find_or_create_by_name(name)
+    
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,24 @@
 class Song
-  attr_accessor
+  attr_accessor :title, :artist
   
   
-  def new_by_filename
-    
+  def self.new_by_filename(filename)
+    song=self.new
+    song.title=filename.split("-")[1]
+    song.artist=filename.split("-")[0]
+    song
   end
   
   def artist(artist_name)
-    
+    artist=artist_name
   end
+  
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist=Artist.new(name)
+    else
+      self.artist.name=name
+    end
+  end  
+  
 end
