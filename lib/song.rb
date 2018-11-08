9f1e73582120a1b8ca38012acbe3da13d4436776
Paris Dolfman <parisdolfman@gmail.com>
2018-11-08 20:25:34 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,17 @@ def initialize(name)
   @name = name
 end
 
+
 def self.new_by_filename(filename)
  song_details = filename.chomp(".mp3").split(" - ")
  song = Song.new(song_details[1])
  song.artist = song_details[0]
  song
+end
+
+def artist_name=(name)
+  self.artist = Artist.find_or_create_by_name(name)
+  artist.add_song(self)
 end
 
 end
