e717cfa656646c36959e882f6c22f078efb48ebe
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-11-09 19:10:34 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    binding.pry
+    @@all << self
+  end
+end
