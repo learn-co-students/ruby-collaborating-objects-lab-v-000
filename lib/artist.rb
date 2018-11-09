98b00ba9a175e5f54231eb7ee3cb8855cd04e040
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-11-09 20:10:33 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,8 +14,27 @@ class Artist
     @songs << song
   end
 
+def self.all
+  @@all
+end
+
   def save
-    binding.pry
     @@all << self
   end
+
+  def self.find_or_create_by_name(name)
+    found_artist = self.all.find {|artist| artist.name == name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(name)
+      new_artist.save
+      new_artist
+    end
+  end
+
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
+
 end
