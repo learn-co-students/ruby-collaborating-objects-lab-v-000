eb86805846c74876f5f0d1510b07f93e0b71ba68
Paris Dolfman <parisdolfman@gmail.com>
2018-11-14 21:39:04 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,12 @@
 
-class Artist
-attr_accessor :name, :songs
+class Artist #creating a class
+attr_accessor :name, :songs #class has two attributes when instantiated
 
-@@all = []
+@@all = []  #empty hash to collect @@all
 
-def initialize(name)
-  @name = name
-  @songs = []
+def initialize(name) #object is initialized with an argument name
+  @name = name #set argument to equal variable
+  @songs = [] #set variable to equal empty hash
 end
 
 def add_song(song)
@@ -14,26 +14,26 @@ def add_song(song)
 end
 
 def self.all #instead of def all which is an instance method we use self to make it a class method
-  @@all
+  @@all #making @@all available to be used as a method when calling .all
 end
 
 def save
-  @@all << self
+  @@all << self #shoveling each instantiated object's self onto the empty hash created above
 end
 
-def self.find_or_create_by_name(name)
-  @@all.find do |name|
-    if name == Artist.name
-      Artist.name
+def self.find_or_create_by_name(name) #argument of name to be found or created
+  @@all.find do |name|  #iterating over each obect in @@all and setting each to equal 'name'
+    if name == Artist.name #if the name is equal to the artists name already created
+      Artist.name #return the artists name
     else
-      name = Artist.new(name)
+      name = Artist.new(name) #if not, let the name be equal to a new artist with the argument originally provided
     end
   end
 end
 
 
 def print_songs
-  puts @songs.collect {|song| song.name}
+  puts @songs.collect {|song| song.name} #iterates over each variable in @songs array, names each object 'song' and returns name
 
 end
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,21 @@
-class Song
-attr_accessor :name, :artist
+class Song #creates a class
+attr_accessor :name, :artist #each class created has two attributes
 
-def initialize(name)
-  @name = name
+def initialize(name) #each object is initialized with a name
+  @name = name #sets the name to a variable
 end
 
-def artist_name=(name)
+def artist_name=(name) #writer method to set variable to equal argument
   self.artist = Artist.find_or_create_by_name(name)
   artist.add_song(self)
 end
 
+
 def self.new_by_filename(filename)
  song_details = filename.chomp(".mp3").split(" - ")
  song = Song.new(song_details[1])
- song.artist_name = song_details[0]
+ song.artist_name = Artist.find_or_create_by_name(song_details[0])
+ #song.artist_name = song_details[0] #this was the one that was orignally passing with only 5 errors left
  song
 end
 
