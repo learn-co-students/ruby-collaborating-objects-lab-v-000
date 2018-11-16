bdebcc6f7566ef7eb3515c0aa7f40d60ae745124
Paris Dolfman <parisdolfman@gmail.com>
2018-11-16 17:17:27 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,8 +9,8 @@ def initialize(name) #object is initialized with an argument name
   @songs = [] #set variable to equal empty hash
 end
 
-def add_song(song)
-  @songs << song
+def add_song(song) #argument of song to be added to array
+  @songs << song #song added to array defined above
 end
 
 def self.all #instead of def all which is an instance method we use self to make it a class method
@@ -21,12 +21,12 @@ def save
   @@all << self #shoveling each instantiated object's self onto the empty hash created above
 end
 
-def self.find_or_create_by_name(name)
-self.find(name) ? self.find(name) : self.create(name)
+def self.find_or_create_by_name(name) #using self to operate on instantiated Artist object and passing in argument to be operated on
+self.find(name) ? self.find(name) : self.create(name) #ternary operator; working on objects self are we able to use the find method below to match the name? if so return the found name else create using method below and name passed
 end
 
-def self.find(name)
-  self.all.find {|artist| artist.name == name}
+def self.find(name) #operating on the objects self passing in argument to be compared
+  self.all.find {|artist| artist.name == name} #using find method on all selves, iterating over all selves in @@all, calling each artist and asking if artist.name is equal to name passed as argument
 end
 
 def self.create(name)
