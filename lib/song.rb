2601266b6dbc14a843aabe3d154b0408a6a275b3
Paris Dolfman <parisdolfman@gmail.com>
2018-11-16 17:05:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,9 @@ def initialize(name) #each object is initialized with a name
   @name = name #sets the name to a variable
 end
 
-def artist_name=(name) #writer method
-  self.artist = Artist.find_or_create_by_name(name)
-  artist.add_song(self)
+def artist_name=(name) #writer method sets variable artist_name to equal argument
+  self.artist = Artist.find_or_create_by_name(name) # find_or_create_by_name method used on Artist class with original name argument passed in is equal to self of artist of song object created
+  artist.add_song(self) #add_song method used on artist of song object created #why does passing self work with this line?
 end
 
 
