c00ec038fd962bf98450a45939b727805913c745
Paris Dolfman <parisdolfman@gmail.com>
2018-11-19 19:51:43 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,9 +6,9 @@ def initialize(path) #initialized w/ a path to parse MP3 files from
  @path = path # sets argument to variable
 end
 
-#def files
- #Dir.glob(*argument of pattern here to find MP3 file) {block here is to execute on files returned by glob}
-#end
+def files
+  Dir.glob('*.mp3').collect {|file|}
+end
 
 def import
 
