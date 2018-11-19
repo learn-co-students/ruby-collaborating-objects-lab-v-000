9b937ad3c3539e71648e6c787d6b585ae933d52f
Paris Dolfman <parisdolfman@gmail.com>
2018-11-19 20:39:48 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,11 +11,16 @@ end
 def files
 #binding.pry
  @files = Dir.glob("#{@path}/*.mp3").collect {|file| file.gsub("#{@path}/", "")}
-
+#setting what we access to @file; accessing the directory and selecting files according to argument;
+#argument interpolates the @path to say 'match what path given in argument ending in mp3.' ex: ("#{@path}/*.mp3")
+#we then call collect on these matching files and iterate over the new array produced calling each item 'file'
+#once we've named these as file we operate on them using .gsub to substitute the @path ex: ("#{@path}/")
+#with what we have written in the .gsub argument after the comma ex: (, "")
 end
 
 def import
-
+ files.each {|file| Song.new_by_filename(file)} #iterating over each file produced in method above, naming each item array file and
+ #producing a new Song object with new_by_filename method; creation information is passed via file argument
 end
 
 
