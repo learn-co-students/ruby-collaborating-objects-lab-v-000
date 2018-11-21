1931ca75b1fc94b1d53885b63a06b14109a4db1c
ktochow1 <ktochow1@binghamton.edu>
2018-11-21 01:49:16 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist 
+ 
+  attr_accessor :name, :songs
+  
+  @@all = []
+
+ 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.find_or_create_by_name(artist_name)
+    artist = @@all.find {|artist| artist_name == artist.name} ? Artist.name : Artist.new(name)
+  end
+  
+  def print_songs
+   @songs.sort.each do |song|
+     puts song.name
+   end
+     
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song 
+  attr_accessor :artist
+  
+  def initialize(name)
+    @name = name
+  end
+  
+end
