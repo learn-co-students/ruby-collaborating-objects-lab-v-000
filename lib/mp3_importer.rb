78d71e0de3e52a71ae3ae3f21e0c430bc28c1cd1
Sarah Paz <sarah.paz@gmail.com>
2019-01-30 02:02:01 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,18 +1,17 @@
 class MP3Importer
-  attr_accessor :path
+  # parses a directory of files
+  # sends file names to a song class
+  attr_accessor :path, :path
 
   def initialize(path)
     @path = path
   end
 
-  def files
-    def size
+  def import
 
-    end
+  end
+  def new_by_filename(file_name)
+
   end
 
-  def import(filenames)
-    Dir.filenames('./spec/fixtures') {|file| file.split(" - ")}
-    song = file.new
-  end
 end
