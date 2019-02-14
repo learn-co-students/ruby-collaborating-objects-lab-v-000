09c98b7b4cf55ed5ffbe2670368911762379b491
Kimberly Aviles <01010110.v@gmail.com>
2019-02-14 17:10:59 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,13 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def self.files
+  end
+
+  def self.import
+  end
+end #<----class end
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,16 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
+  end
+
+  def artist
+    @artist = artist
+  end
+
+  def new_by_filename(file_name)
+    song = Song.new(file_name)
   end
 
 end
