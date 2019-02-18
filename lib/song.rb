bc49cc80bd17a00371d2bc5bb2f847457126bfb9
Niusha H <niusha.hadziomerovic@gmail.com>
2019-02-18 18:49:32 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+  attr_accessor :artist
+ 
+  # other methods
+ 
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+end
