339a3bde38ca50d081b96beeb5ac4753b2f024b0
Kimberly Aviles <01010110.v@gmail.com>
2019-02-25 01:00:14 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,15 +9,21 @@ class Song # create songs & send artist name STRING to ARTIST CLASS
     @name
   end
 
-  def artist()
+  def artist
     @artist
   end
 
-  def new_by_filename(file)
+  def self.new_by_filename(filename)
     #parse the file for the song and artist
+    file_split = filename.split(/-/)
+    artist_name = file_split[0].strip
+    song_name = file_split[1].strip
     #create new song instance
-    #assciate new song with an artist - helpermethod Song.artist()
+    song_object = Song.new(song_name)
+    #associate new song with an artist - helpermethod Song.artist()
+    song_object.artist.name = artist_name
     #return new song instance
+    song_object
   end
 
 end
