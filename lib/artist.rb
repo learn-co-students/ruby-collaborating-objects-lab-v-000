e66742f07e4974ec1c18ddc60fe411876fe90661
Dan Suarez <suarez.daniel101@gmail.com>
2019-04-20 21:15:51 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,14 +25,24 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if self.all.eql?(self.name)
-      # find { |a| a == self.name }
-      # a1.eql?(a2)
-      # self.name
-    elsif self.name.nil?
-      artist = Artist.new(name)
-      artist
+    # if self.all.eql?(self.name)
+      if @@all.detect(self.name)
+      else Artist.new(name)
     end
   end
+# This class method should take the name that is passed in (remember, it will be a string), and do one of two things.
+#   1. Find the artist instance that has that name
+# or
+# 2. create one if it doesn't exist.
+#   Either way, the return value of the method will be an instance of an artist with the name attribute filled out.
 
+
+  # def self.find_or_create_by_name(name)
+  #     if self.name == self.name
+  #       self
+  #     elsif self.name.nil?
+  #       artist = Artist.new(name)
+  #       artist
+  #     end
+  #   end
 end
