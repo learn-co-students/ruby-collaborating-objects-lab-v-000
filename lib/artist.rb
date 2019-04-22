11648fd8f90cbb38d77f28a01d443acc86f6c3d5
Dan Suarez <suarez.daniel101@gmail.com>
2019-04-22 15:29:11 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,37 +25,19 @@ class Artist
   @@all << self
   end
 
-  # def self.find_or_create_by_name(name)
-  #     if @@all.detect { |a| a.instance_variable_get(:@name) }
-  #     else artist = Artist.new(name)
-  #   end
-  # end
-
-
-
-  # def add_song_by_name(name)
-  #   song = Song.new(name)
-  #   @songs << song
-  #   song.artist = self
-  # end
-
-  # binding.pry
-
 # This class method should take the name that is passed in (remember, it will be a string), and do one of two things.
 #   1. Find the artist instance that has that name
 # or
 # 2. create one if it doesn't exist.
 #   Either way, the return value of the method will be an instance of an artist with the name attribute filled out.
 
-
    def self.find_or_create_by_name(name)
-        # if @@all.include?(self.name)
-        #   self.name
-      if self.name == self.name
-      @@all.detect { |a| a.instance_variable_get(:@name) }
-        elsif self.name.nil?
-         artist = Artist.new(name)
-          artist
-       end
-     end
+      if self.name.eql?(self.name)
+      # if self.name == self.name
+        @@all.detect { |a| a.instance_variable_get(:@name) }
+      else Artist.new(name)
+        # artist
+      end
+   end
+     # binding.pry
 end
