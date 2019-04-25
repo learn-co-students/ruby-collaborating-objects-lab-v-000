d28858d8d54f1ceb9d5559f7d9f63f2960de0b31
Dan Suarez <suarez.daniel101@gmail.com>
2019-04-25 03:35:27 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,13 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :file_name
 
   def initialize(path)
     @path = path
+    @file_name = file_name
   end
 
   def files
+    # file = File.basename(".mp3")
     file = File.basename(".mp3")
     file
 
