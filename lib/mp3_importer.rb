10df8b0ebdf4ca92d01a253a710a737b14115f96
Dan Suarez <suarez.daniel101@gmail.com>
2019-04-25 03:16:14 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,17 +6,10 @@ class MP3Importer
   end
 
   def files
-    # file = File.dirname("path/mp3")
-    # file
-    # All files containing "spec" in the name
-    Dir.glob(".mp3*")
+    file = File.basename(".mp3")
+    file
+
+    # Dir["/path/*.mp3"].each {|file| require file }
   end
 
 end
-
-
-
-# def find_files_in_current_directory
-#   entries = Dir.entries(".")
-#   entries.reject { |entry| File.directory?(entry) }
-# end
