b00668fcdab50d9f89e42657fd69c72e0f420980
Dan Suarez <suarez.daniel101@gmail.com>
2019-04-26 01:51:10 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,6 +8,10 @@ class MP3Importer
   def files
     file = File.basename(path)
     file
+    # file = Dir.entries("./spec/fixtures/mp3s")
+    # file = Pathname("./spec/fixtures/mp3s")
   end
 
 end
+
+# rb_file_s_size - .mp3
