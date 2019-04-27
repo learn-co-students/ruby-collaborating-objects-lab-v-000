57225413205c65bd2003d5600b864c807ce65ead
Dan Suarez <suarez.daniel101@gmail.com>
2019-04-27 02:58:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
 
   def self.new_by_filename(file_name)
     new_instance = Song.new(file_name)
-    new_instance.split
+    new_instance.split()
     # new_instance.instance_variable_get("@#{file_name}")
     # String1.scan(/<([^>]*)>/).last.first
   end
