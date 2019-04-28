2e843b0a1fa88c03a663f639607971d36392d3c5
Dan Suarez <suarez.daniel101@gmail.com>
2019-04-28 22:32:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -12,10 +12,8 @@ class Song
   end
 
   def self.new_by_filename(file_name)
-    new_instance = Song.new(file_name)
-    new_instance.split()
-    # new_instance.instance_variable_get("@#{file_name}")
-    # String1.scan(/<([^>]*)>/).last.first
+    new_instance = Song.new(file_name.scan(/(\w+\s\w+\s\w+)/).join)
+    # new_instance.artist = file_name
   end
 
 end
