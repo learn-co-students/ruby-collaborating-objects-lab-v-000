b117796403b4f356288c8e9d5114903714783f21
Dan Suarez <suarez.daniel101@gmail.com>
2019-05-08 23:15:36 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,6 +9,9 @@ class MP3Importer
 
   def files
     Dir.entries(path).select {|f| !File.directory? f}
+  end
+
+  # Dir.entries(path).select {|f| File.file?(f)}
     # File.basename(".mp3")
     # Pathname.glob(path)
     # Dir.entries(path).reject {|f| File.directory? f}
@@ -16,10 +19,16 @@ class MP3Importer
     # file = Pathname("mp3")
     # file = Dir.entries("./spec/fixtures/mp3s")
     # file = Pathname("./spec/fixtures/mp3s")
-  end
 
   def import
-     4.times {Song.new_by_filename(file_name)}
+    #  4.times {Song.new_by_filename(file_name)} "this works but hardcoded to 4 times"
+    files.each {|filename| Song.new_by_filename(filename)}
   end
-
 end
+
+# Example:
+# class MP3Importer
+#   def import(list_of_filenames)
+#     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+#   end
+# end
