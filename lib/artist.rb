2dab35a48bd339c3ebd9f5b63339d63ba5b95756
Dan Suarez <suarez.daniel101@gmail.com>
2019-05-09 03:49:15 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,7 +16,7 @@ class Artist
 
   def add_song(song)
     @songs << song
-    # song.artist = self
+    save
   end
 
   def self.all
--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    self.artist.add_song(self)
+    self.artist.add_song(name)
   end
   # binding.pry
 end
