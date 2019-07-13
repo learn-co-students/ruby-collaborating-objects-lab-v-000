68f73ffadeb816f89960aff03e80e2bcad917d58
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-07-13 20:42:08 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,6 +31,14 @@ class Artist
     find_by_name(name) || create_by_name(name)
   end
 
+  def add_song(song)
+    @songs<<song
+  end
+
+  def save
+    self.class.all<<self
+  end
+
   def print_songs
     @songs.each{|song| puts song.name}
   end
