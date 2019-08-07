209e5fd554ef5d0e0004121453f6f9090876e08c
Zain <zain@halai.us>
2019-08-07 15:03:19 -0500

    Update artist.rb

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,7 +26,7 @@ class Artist
 	end
 
 	def self.find_or_create_by_name(name)
-		self.find_artist(name) ? self.find_artist(name) : self.create(name)
+		self.find_artist(name) || self.create(name)
 	end
 
 	def songs
