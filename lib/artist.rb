20d25891e82912f0a7524b896a00f8dddf4c3610
Olivia Kesler <oakesler@protonmail.ch>
2019-08-22 20:46:51 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,6 @@ class Artist
   
   def songs
     Song.all.select {|song| song.artist == self}
-    #binding.pry
   end
   
   def self.all
--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,28 @@
 class Song 
   
-  attr_accessor :name, :artist, :songs #:filename
+  attr_accessor :name, :artist, :songs 
   
   @@all = []
   
   def initialize(name = @name)
     @name = name
     @@all << self
-    #@@all << name 
   end
   
   def self.all
     @@all
   end
   
-  def self.new_by_filename(filename)
-    song = self.new(filename.split(" - ")[1])
-    artist_name=(filename.split(" - ")[0])
-    song.artist = artist_name
-    song
-  end
-  
   def artist_name=(name)
     guy = Artist.find_or_create_by_name(name)
     guy.add_song(self)
     guy
+  end
+  
+  def self.new_by_filename(filename)
+    song = self.new(filename.split(" - ")[1])
+    song.artist_name = filename.split(" - ")[0]
+    song
   end
 end
   
