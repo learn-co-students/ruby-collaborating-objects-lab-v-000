a0cbe341005d7b10d0060410b73d74c3b2ced3da
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 19:57:03 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+class Artist
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def self.find_or_create_by_name(name)
+    @@all.each do |artist| 
+      if artist.name == name
+        return artist
+      end
+    end
+    self.new(name)
+  end
+  
+  def print_songs
+    Song.all.each { |song| puts song.name if song.artist == self }
+  end
+    
+  
+end
