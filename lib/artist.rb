726a589b8c5840a3f11ada75eec3cd3b3a5e16f1
Meg Gutshall <meghan.gutshall@gmail.com>
2020-03-23 10:17:58 -0400

    Adjust comments

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,7 @@ class Artist
     @@all
   end
 
-  # Instance method that takes a song instance from the Song class
-  # and adds it to the artist's songs array
+  # Instance method that takes a song instance from the Song class and adds it to the artist's songs array
   def add_song(song)
     @songs << song
   end
@@ -24,11 +23,7 @@ class Artist
     @@all << self
   end
 
-  # Find the artist instance with the value "name" or create one if
-  # it doesn't already exist. Iterate over the @@all class variable to
-  # find each artist instance. If the artist instance's name is equal
-  # to the argument name, return that artist instance. If not, create
-  # a new artist instance using the name in the method argument.
+  # Find the artist instance with the value "name" or create one if it doesn't already exist. Iterate over the @@all class variable to find each artist instance. If the artist instance's name is equal to the argument name, return that artist instance. If not, create a new artist instance using the name in the method argument.
   def self.find_or_create_by_name(name)
     @@all.find {|artist| return artist if artist.name == name}
     new_artist = Artist.new(name)
@@ -36,9 +31,7 @@ class Artist
     new_artist
   end
 
-  # Access a list of the artist instance's songs by the @songs
-  # instance variable, then iterate over the array, printing
-  # each song name with a line break following.
+  # Access a list of the artist instance's songs by the @songs instance variable, then iterate over the array, printing each song name with a line break following.
   def print_songs
     @songs.each {|song| puts song.name}
   end
--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,7 @@ class Song
     @name = name
   end
 
-  # Create a new song instance using the files imported from the MP3Importer class.
-  # Split the file_name and add the song title as the argument for initializing
-  # a new song instance. Save the artist name into the artist_name variable.
-  # Return and call the #artist_name=(artist_name) method on the newly created
-  # song instance.
+  # Create a new song instance using the files imported from the MP3Importer class. Split the file_name and add the song title as the argument for initializing a new song instance. Save the artist name into the artist_name variable. Return and call the #artist_name=(artist_name) method on the newly created song instance.
   def self.new_by_filename(file_name)
     song = Song.new(file_name.split(" - ")[1])
     song.artist_name=(file_name.split(" - ")[0])
@@ -24,12 +20,7 @@ class Song
   #   new_song
   # end
 
-  # This is a helper method meant to associate the new song instance with the
-  # artist from the artist_name variable (which was parsed from the file_name
-  # variable). Use the Artist class's #find_or_create_by_name method with an
-  # argument of artist_name to create a new artist object and assign it to the
-  # current instance of song. Add the current instance of song to the artist
-  # instance using the Artist class's #add_song method.
+  # This is a helper method meant to associate the new song instance with the artist from the artist_name variable (which was parsed from the file_name variable). Use the Artist class's #find_or_create_by_name method with an argument of artist_name to create a new artist object and assign it to the current instance of song. Add the current instance of song to the artist instance using the Artist class's #add_song method.
   def artist_name=(artist_name)
     self.artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(self)
